3969ce386ffe9837ca769be68d4bf105d042d95c
LisandrOviedo <lisandroviedo@hotmail.com>
2024-03-12 13:21:19 -0400

    Añadidas ruta del formulario

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
+
 import { Routes, Route, useLocation } from "react-router-dom";
+
 import { Landing } from "./components/Landing/Landing";
+import { Form } from "./components/Form/Form";
 
 function App() {
   const { pathname } = useLocation();
@@ -8,6 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/form/:cedula" element={<Form />} />
     </Routes>
   );
 }
