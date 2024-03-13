db2753b0e79cc91a20e1d74e51cd2773b3fcf7eb
LisandrOviedo <lisandroviedo@hotmail.com>
2024-03-13 17:10:32 -0400

    Ajustes visuales

--- Client/src/components/UI/Button.jsx
+++ Client/src/components/UI/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button(props) {
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      className="mb-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
       {...props}
     ></button>
   );
--- Client/src/components/UI/Input.jsx
+++ Client/src/components/UI/Input.jsx
@@ -2,7 +2,7 @@ export default function Input(props) {
   return (
     <div className="mt-2">
       <input
-        className="p-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        className="p-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 focus:ring-2 focus:ring-inset focus:ring-[#002846] sm:text-sm sm:leading-6"
         {...props}
       />
     </div>
