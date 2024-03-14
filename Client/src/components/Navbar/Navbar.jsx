983a0e794f8b925b19cd272b9a815c4dfeecf9c9
LisandrOviedo <lisandroviedo@hotmail.com>
2024-03-14 16:47:31 -0400

    Corrección del navbar para en un futuro añadir una nueva condición cuando el admin es logueado de forma correcta se muestren nuevas opciones

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -62,7 +62,7 @@ export function Navbar() {
                 </li>
               </>
             )}
-            {pathname.startsWith("/admin/") && (
+            {pathname === "/admin/login" && (
               <>
                 <li>
                   <Link
