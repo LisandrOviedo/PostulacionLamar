106e812b49a6bb8aab3f514cb850ae2d61b9b002
LisandrOviedo <lisandroviedo@hotmail.com>
2024-03-14 15:48:13 -0400

    Añadida ruta para el login del administrador
    Añadida la validación para rutas no encontradas, te redirecciona a la ruta principal

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 
 import { Form } from "./components/Form/Form";
 import { Landing } from "./components/Landing/Landing";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Login } from "./components/Admin/Login/Login";
 
 function App() {
   const { pathname } = useLocation();
+
+  function NotFound() {
+    return <Navigate to="/" />;
+  }
 
   return (
     <div>
@@ -15,6 +20,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/form/:cedula" element={<Form />} />
+        <Route path="/admin/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
