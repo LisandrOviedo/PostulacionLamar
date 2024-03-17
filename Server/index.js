c4f8535d50ae164baf20ae756b96d4c5551875f5
lisandroviedo <lisandroviedo@hotmail.com>
2024-03-17 14:07:38 -0400

    Encendido del servidor

--- Server/index.js
+++ Server/index.js
@@ -1,1 +1,14 @@
-console.log("Server Encendido")
+require("dotenv").config();
+
+const server = require("./src/server.js");
+const { conn } = require("./src/db.js");
+const PORT = process.env.PORT_SERVER || 3001;
+
+conn
+  .sync({ force: true })
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.error(error));
