d810197665dea45d9c8db0ce9a166b29b11ea9df
lisandroviedo <lisandroviedo@hotmail.com>
2024-03-17 15:05:07 -0400

    Relaciones de tablas añadidas

--- Server/src/db.js
+++ Server/src/db.js
@@ -37,6 +37,37 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { Usuario, Empleado, Areas_Interes, Curriculo } = sequelize.models;
 
 // RELACIONES DE MODELOS (TABLAS)
+// Empleado 1:1 Curriculo
+Empleado.hasOne(Curriculo, {
+  foreignKey: {
+    allowNull: false,
+    name: "empleado_id",
+    onDelete: "RESTRICT", // Configurar el comportamiento de eliminación a RESTRICT
+    onUpdate: "RESTRICT", // Configurar el comportamiento de actualización a RESTRICT
+  },
+});
+Curriculo.belongsTo(Empleado, {
+  foreignKey: {
+    allowNull: false,
+    name: "empleado_id",
+  },
+});
+
+// Areas_Interes 1:N Direcciones
+Areas_Interes.hasMany(Curriculo, {
+  foreignKey: {
+    allowNull: false,
+    name: "area_interes_id",
+    onDelete: "RESTRICT", // Configurar el comportamiento de eliminación a RESTRICT
+    onUpdate: "RESTRICT", // Configurar el comportamiento de actualización a RESTRICT
+  },
+});
+Curriculo.belongsTo(Areas_Interes, {
+  foreignKey: {
+    allowNull: false,
+    name: "area_interes_id",
+  },
+});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
