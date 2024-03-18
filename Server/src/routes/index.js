1d37f3cb36aaab2c8c81e0b2bbc4a5abdb95062c
LisandrOviedo <lisandroviedo@hotmail.com>
2024-03-18 17:05:25 -0400

    Corrección de rutas

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const areasinteres = require("./areas_interes_routes");
-const cargotitulos = require("./cargo_titulos_routes");
+const cargostitulos = require("./cargo_titulos_routes");
 const curriculos = require("./curriculos_routes");
 const empleados = require("./empleados_routes");
 const experiencias = require("./experiencias_routes");
@@ -9,7 +9,7 @@ const usuarios = require("./usuarios_routes");
 const router = Router();
 
 router.use("/areasinteres", areasinteres);
-router.use("/cargotitulos", cargotitulos);
+router.use("/cargostitulos", cargostitulos);
 router.use("/curriculos", curriculos);
 router.use("/empleados", empleados);
 router.use("/experiencias", experiencias);
