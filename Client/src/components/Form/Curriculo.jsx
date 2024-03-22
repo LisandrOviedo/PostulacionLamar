813a2ecd3dfe2afb3cec1b997c443671795a1198
LisandrOviedo <lisandroviedo@hotmail.com>
2024-03-22 16:05:28 -0400

    Añadidos campos faltantes de la base de datos Ajustes visuales, ahora muestra 3 campos por fila Removidas líneas innecesarias Desarrollando botón para registrar curriculo

--- Client/src/components/Form/Curriculo.jsx
+++ Client/src/components/Form/Curriculo.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import axios from "axios";
+
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { useSelector, useDispatch } from "react-redux";
 
@@ -9,10 +12,14 @@ import { Button, Input, Label, Select, Title } from "../UI";
 
 export function Curriculo() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const empleado = useSelector((state) => state.empleado);
   const areas_interes = useSelector((state) => state.areas_interes);
 
-  const [data, setData] = useState({
+  const URL_SERVER = import.meta.env.VITE_URL_SERVER;
+
+  const [datos, setDatos] = useState({
     empleado_id: empleado.empleado.empleado_id,
     grado_instruccion: "Basico",
     titulo_obtenido: "",
@@ -22,7 +29,6 @@ export function Curriculo() {
     disponibilidad_viajar: "true",
     disponibilidad_cambio_residencia: "true",
     ruta_pdf: "",
-    estado: "Pendiente por revisar",
   });
 
   const handlePDF = (event) => {
@@ -51,11 +57,21 @@ export function Curriculo() {
   const handleInputChange = (event) => {
     const { name, value } = event.target;
 
-    setData({ ...data, [name]: value });
+    setDatos({ ...datos, [name]: value });
   };
 
-  const handleCreateCurriculo = (event) => {
+  const handleAddArea = (event) => {};
+
+  const handleCreateCurriculo = async (event) => {
     event.preventDefault();
+
+    const { data } = await axios.post(`${URL_SERVER}/curriculos`, datos);
+
+    if (data.empleado_id) {
+      return alert("Curriculo registrado");
+    }
+
+    return alert(data);
   };
 
   return (
@@ -64,12 +80,12 @@ export function Curriculo() {
       <hr className="w-[80%] h-0.5 my-5 bg-gray-300 border-0 m-auto" />
 
       <form>
-        <div className="grid gap-6 md:grid-cols-2 mt-5">
+        <div className="grid gap-6 md:grid-cols-3 mt-5 mb-5">
           <div>
-            <Label>Grado de instrucción</Label>
+            <Label>Grado de instrucción más alta</Label>
             <Select
               name="grado_instruccion"
-              value={data.grado_instruccion}
+              value={datos.grado_instruccion}
               onChange={handleInputChange}
             >
               <option value="Basico">Básico</option>
@@ -87,7 +103,7 @@ export function Curriculo() {
               type="text"
               name="titulo_obtenido"
               placeholder="Ingrese el nombre de su título obtenido"
-              value={data.titulo_obtenido}
+              value={datos.titulo_obtenido}
               onChange={handleInputChange}
             />
           </div>
@@ -97,44 +113,97 @@ export function Curriculo() {
               type="text"
               name="centro_educativo"
               placeholder="Ingrese el nombre del centro educativo donde obtuvo su título"
-              value={data.centro_educativo}
+              value={datos.centro_educativo}
               onChange={handleInputChange}
             />
           </div>
           <div>
             <Label>Indica cuál es tu área de interés laboral</Label>
-            <Select
-              name="area_interes_id"
-              value={data.area_interes_id}
-              onChange={handleInputChange}
-            >
-              {areas_interes &&
-              areas_interes.areas_interes &&
-              areas_interes.areas_interes.length > 0
-                ? areas_interes?.areas_interes.map(
-                    (area, i) =>
-                      area.activo && (
-                        <option key={i} value={area.area_interes_id}>
-                          {area.nombre}
-                        </option>
-                      )
-                  )
-                : null}
-            </Select>
+            <div className="flex gap-4 w-full items-start">
+              <Select
+                name="area_interes_id"
+                value={datos.area_interes_id}
+                onChange={handleInputChange}
+                className="inline-block"
+              >
+                {areas_interes &&
+                areas_interes.areas_interes &&
+                areas_interes.areas_interes.length > 0
+                  ? areas_interes?.areas_interes.map(
+                      (area, i) =>
+                        area.activo && (
+                          <option key={i} value={area.area_interes_id}>
+                            {area.nombre}
+                          </option>
+                        )
+                    )
+                  : null}
+              </Select>
+              <Button className="m-0" onClick={handleAddArea}>
+                Agregar
+              </Button>
+            </div>
           </div>
-
           <div>
-            <Label>Área de interés (Otro)</Label>
+            <Label>Otra área de interés (Opcional)</Label>
             <Input
               type="text"
               name="area_interes_otro"
               placeholder="Ingrese el nombre del área de interés"
-              value={data.area_interes_otro}
+              value={datos.area_interes_otro}
               onChange={handleInputChange}
             />
           </div>
           <div>
-            <Label>¿Disponibilidad para viajar?</Label>
+            <Label>Adjunte su resumen curricular (PDF)</Label>
+            <input
+              className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+              aria-describedby="file_input_help"
+              id="file_input"
+              type="file"
+              accept="application/pdf"
+              onChange={handlePDF}
+              name="ruta_pdf"
+              value={datos.ruta_pdf}
+            />
+            <p
+              className="mt-1 text-sm text-red-600 dark:text-gray-300"
+              id="file_input_help"
+            >
+              ¡Solo archivos en formato PDF!
+            </p>
+          </div>
+          <div className="md:col-span-3">
+            <table className="w-full mx-auto text-sm text-left rtl:text-right dark:text-gray-400">
+              <thead className="text-xs uppercase bg-blue-600 dark:bg-gray-700 dark:text-gray-400">
+                <tr className="text-white">
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex items-center">
+                      Areas de interés seleccionadas
+                    </div>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex items-center">Acción</div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="bg-gray-400 border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td className="px-4 py-4">Area 1</td>
+                  <td className="px-4 py-4">
+                    <a
+                      href="#"
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Borrar
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div>
+            <Label>¿Posees disponibilidad para viajar?</Label>
             <div className="mt-4">
               <input
                 id="disponibilidad_viajar_si"
@@ -161,7 +230,7 @@ export function Curriculo() {
             </div>
           </div>
           <div>
-            <Label>¿Disponibilidad para cambio de residencia?</Label>
+            <Label>¿Posees disponibilidad para cambio de residencia?</Label>
             <div className="mt-4">
               <input
                 id="disponibilidad_cambio_residencia_si"
@@ -193,27 +262,12 @@ export function Curriculo() {
               </label>
             </div>
           </div>
-          <div>
-            <Label>Adjunte su resumen curricular (PDF)</Label>
-            <input
-              className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-              aria-describedby="file_input_help"
-              id="file_input"
-              type="file"
-              accept="application/pdf"
-              onChange={handlePDF}
-              name="ruta_pdf"
-              value={data.ruta_pdf}
-            />
-            <p
-              className="mt-1 text-sm text-red-600 dark:text-gray-300"
-              id="file_input_help"
-            >
-              ¡Solo archivos en formato PDF!
-            </p>
+          <div className="flex items-end">
+            <Button className="m-0" onClick={handleCreateCurriculo}>
+              Continuar
+            </Button>
           </div>
         </div>
-        <Button onClick={handleCreateCurriculo}>Continuar</Button>
       </form>
     </div>
   );
