5a5ccf74363e035828e1346d593df73f29b05204
LisandrOviedo <lisandroviedo@hotmail.com>
2024-03-26 16:50:20 -0400

    Eliminados archivos innecesarios con la nueva estructura de la base de datos

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,6 +1,5 @@
 const { Router } = require("express");
 const areasinteres = require("./areas_interes_routes");
-const cargostitulos = require("./cargo_titulos_routes");
 const curriculos = require("./curriculos_routes");
 const empleados = require("./empleados_routes");
 const experiencias = require("./experiencias_routes");
