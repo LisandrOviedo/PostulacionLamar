4b73858947ba13bc4587308b38e2df5f007a7d6b
LisandrOviedo <lisandroviedo@hotmail.com>
2024-03-26 15:45:53 -0400

    Eliminación de campos y/o líneas innecesarias
    Refactorización de código
    
    Añadidos los campos de experiencia
    
    Ajustes visuales

--- Client/src/components/Form/Curriculo.jsx
+++ Client/src/components/Form/Curriculo.jsx
@@ -22,16 +22,14 @@ export function Curriculo() {
   const [datosCurriculo, setDatosCurriculo] = useState({
     empleado_id: empleado.empleado.empleado_id,
     grado_instruccion: "Basico",
-    titulo_obtenido: "",
+    titulos_obtenidos: [],
     disponibilidad_viajar: "true",
     disponibilidad_cambio_residencia: "true",
-    areas_interes: [], // ID's de las áreas
-    experiencia_tipo: "Ninguno",
-    cargos_titulos: [],
-    experiencia_duracion: "Menos de 1 año",
+    areas_interes: [],
+    experiencias: [],
   });
 
-  const [isReadOnly, setIsReadOnly] = useState(true);
+  const [isHidden, setIsHidden] = useState(true);
 
   const handlePDF = (event) => {
     const input = event.target;
@@ -51,6 +49,7 @@ export function Curriculo() {
 
   useEffect(() => {
     window.scroll(0, 0);
+
     dispatch(getAllAreasInteres());
   }, []);
 
@@ -60,72 +59,20 @@ export function Curriculo() {
     setDatosCurriculo({ ...datosCurriculo, [name]: value });
   };
 
-  const handleCargoTituloSelected = () => {
-    const select = document.getElementById("cargo_titulo_id");
+  const handleTipoExpSelected = () => {
+    const select = document.getElementById("tipo");
     const name = select.options[select.selectedIndex].text;
-    const cargoTituloOtro = document.getElementById("cargo_titulo_otro");
 
-    if (name === "Otro") {
-      return setIsReadOnly(false);
+    if (name === "Ninguno") {
+      setIsHidden(true);
+      return;
     }
 
-    if (isReadOnly === false) {
-      cargoTituloOtro.value = "";
-      return setIsReadOnly(true);
+    if (isHidden) {
+      setIsHidden(false);
+      return;
     }
   };
-
-  // const handleAddCargoTitulo = (event) => {
-  //   event.preventDefault();
-
-  //   if (datosCurriculo.cargos_titulos.length === 3) {
-  //     return alert("Solo puedes agregar máximo 3 áreas de interés");
-  //   }
-
-  //   const areaOtro = document.getElementById("area_interes_otro");
-  //   const select = document.getElementById("area_interes_id");
-  //   const name = select.options[select.selectedIndex].text;
-  //   const value = select.options[select.selectedIndex].value;
-
-  //   const areaValidatorInclude = datosCurriculo.areas_interes.some(
-  //     (area) => area.area_interes_id === value && area.nombre === name
-  //   );
-
-  //   if (areaValidatorInclude) {
-  //     areaOtro.value = "";
-  //     return alert("Ya has agregado esta área de interés");
-  //   }
-
-  //   if (name === "Otro") {
-  //     if (areaOtro.value === "") {
-  //       areaOtro.focus();
-  //       return alert("Debes escribir el nombre del área que deseas añadir");
-  //     }
-
-  //     setDatosCurriculo({
-  //       ...datosCurriculo,
-  //       areas_interes: [
-  //         ...datosCurriculo.areas_interes,
-  //         { area_interes_id: value, nombre: name, nombre_otro: areaOtro.value },
-  //       ],
-  //     });
-
-  //     areaOtro.value = "";
-  //     select.selectedIndex = 0;
-
-  //     return setIsReadOnly(true);
-  //   }
-
-  //   select.selectedIndex = 0;
-
-  //   return setDatosCurriculo({
-  //     ...datosCurriculo,
-  //     areas_interes: [
-  //       ...datosCurriculo.areas_interes,
-  //       { area_interes_id: value, nombre: name, nombre_otro: "" },
-  //     ],
-  //   });
-  // };
 
   const handleAddArea = (event) => {
     event.preventDefault();
@@ -155,8 +102,142 @@ export function Curriculo() {
     });
 
     select.selectedIndex = 0;
+    return;
+  };
 
-    return setIsReadOnly(true);
+  const handleDeleteArea = (event) => {
+    const rowIndex = event.target.parentNode.parentNode.rowIndex;
+    const updatedAreasInteres = datosCurriculo.areas_interes.filter(
+      (_, index) => index !== rowIndex - 1
+    );
+
+    setDatosCurriculo({
+      ...datosCurriculo,
+      areas_interes: updatedAreasInteres,
+    });
+  };
+
+  const handleAddTituloObtenido = (event) => {
+    event.preventDefault();
+
+    const select = document.getElementById("titulo_obtenido");
+
+    if (!select.value) {
+      select.focus();
+      return alert("Debes ingresar el nombre del título obtenido");
+    }
+
+    let duplicado = false;
+
+    datosCurriculo.titulos_obtenidos.forEach((titulo_obtenido) => {
+      if (titulo_obtenido.toLowerCase() === select.value.toLowerCase()) {
+        duplicado = true;
+        return;
+      }
+    });
+
+    if (duplicado) {
+      select.value = "";
+      select.focus();
+      return alert("Ya has agregado un título obtenido con ese nombre");
+    }
+
+    setDatosCurriculo({
+      ...datosCurriculo,
+      titulos_obtenidos: [...datosCurriculo.titulos_obtenidos, select.value],
+    });
+
+    select.value = "";
+    select.focus();
+    return;
+  };
+
+  const handleAddExperiencia = (event) => {
+    event.preventDefault();
+
+    const tipo = document.getElementById("tipo");
+    const cargo_titulo = document.getElementById("cargo_titulo");
+    const duracion = document.getElementById("duracion");
+    const empresa_centro_educativo = document.getElementById(
+      "empresa_centro_educativo"
+    );
+
+    if (tipo.value === "Ninguno") {
+      return alert("Debes seleccionar un tipo de experiencia");
+    }
+
+    if (!cargo_titulo.value || !empresa_centro_educativo) {
+      cargo_titulo.focus();
+      return alert(
+        "Debes escribir el nombre del cargo o título y el nombre de la empresa o centro educativo"
+      );
+    }
+
+    let duplicado = false;
+
+    datosCurriculo.experiencias.forEach((experiencia) => {
+      if (
+        experiencia.tipo.toLowerCase() === tipo.value.toLowerCase() &&
+        experiencia.cargo_titulo.toLowerCase() ===
+          cargo_titulo.value.toLowerCase() &&
+        experiencia.empresa_centro_educativo ===
+          empresa_centro_educativo.value.toLowerCase()
+      ) {
+        duplicado = true;
+        return;
+      }
+    });
+
+    if (duplicado) {
+      cargo_titulo.value = "";
+      empresa_centro_educativo.value = "";
+      cargo_titulo.focus();
+      return alert(
+        "Ya has agregado una experiencia con ese cargo / título en esa empresa / centro educativo"
+      );
+    }
+
+    setDatosCurriculo({
+      ...datosCurriculo,
+      experiencias: [
+        ...datosCurriculo.experiencias,
+        {
+          tipo: tipo.value,
+          cargo_titulo: cargo_titulo.value,
+          duracion: duracion.value,
+          empresa_centro_educativo: empresa_centro_educativo.value,
+        },
+      ],
+    });
+
+    cargo_titulo.value = "";
+    empresa_centro_educativo.value = "";
+    duracion.selectedIndex = 0;
+    cargo_titulo.focus();
+  };
+
+  const handleDeleteExp = (event) => {
+    const rowIndex = event.target.parentNode.parentNode.rowIndex;
+    const updatedExperiencias = datosCurriculo.experiencias.filter(
+      (_, index) => index !== rowIndex - 1
+    );
+
+    setDatosCurriculo({
+      ...datosCurriculo,
+      experiencias: updatedExperiencias,
+    });
+  };
+
+  const handleDeleteTituloObtenido = (event) => {
+    const rowIndex = event.target.parentNode.parentNode.rowIndex;
+    const updatedTitulosObtenidos = datosCurriculo.titulos_obtenidos.filter(
+      (_, index) => index !== rowIndex - 1
+    );
+
+    setDatosCurriculo({
+      ...datosCurriculo,
+      titulos_obtenidos: updatedTitulosObtenidos,
+    });
   };
 
   const handleCreateCurriculo = async (event) => {
@@ -193,25 +274,13 @@ export function Curriculo() {
     return alert(data);
   };
 
-  const handleDeleteRow = (event) => {
-    const rowIndex = event.target.parentNode.parentNode.rowIndex;
-    const updatedAreasInteres = datosCurriculo.areas_interes.filter(
-      (_, index) => index !== rowIndex - 1
-    );
-
-    setDatosCurriculo({
-      ...datosCurriculo,
-      areas_interes: updatedAreasInteres,
-    });
-  };
-
   return (
     <div className="mt-24 sm:mt-32 h-full flex flex-col px-5 sm:px-10 bg-white static">
       <Title>Datos Currículo</Title>
       <hr className="w-[80%] h-0.5 my-5 bg-gray-300 border-0 m-auto" />
 
       <form>
-        <div className="grid gap-6 md:grid-cols-3 mt-5 mb-5">
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-3 mt-5 mb-5">
           <div>
             <Label htmlFor="grado_instruccion">
               Grado de instrucción más alta obtenida
@@ -241,10 +310,8 @@ export function Curriculo() {
                 type="text"
                 name="titulo_obtenido"
                 placeholder="Ingrese el nombre del título"
-                value={datosCurriculo.titulo_obtenido}
-                onChange={handleInputChangeCurriculo}
               />
-              <Button className="m-0" onClick={handleAddArea}>
+              <Button className="m-0" onClick={handleAddTituloObtenido}>
                 Agregar
               </Button>
             </div>
@@ -266,6 +333,41 @@ export function Curriculo() {
             >
               ¡Solo archivos en formato PDF!
             </p>
+          </div>
+          <div className="md:col-span-3">
+            <table className="w-full mx-auto text-sm text-left rtl:text-right dark:text-gray-400">
+              <thead className="text-xs uppercase bg-blue-600 dark:bg-gray-700 dark:text-gray-400">
+                <tr className="text-white">
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex items-center">
+                      Títulos obtenidos agregados
+                    </div>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex items-center">Acción</div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {datosCurriculo.titulos_obtenidos.map((titulo_obtenido, i) => (
+                  <tr
+                    key={i}
+                    className="bg-gray-400 border-b dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <td className="px-4 py-4">{titulo_obtenido}</td>
+                    <td className="px-4 py-4">
+                      <a
+                        href="#"
+                        className="font-medium text-red-600 dark:text-blue-500"
+                        onClick={handleDeleteTituloObtenido}
+                      >
+                        Borrar
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
           <div>
             <Label htmlFor="area_interes_id">
@@ -373,9 +475,6 @@ export function Curriculo() {
                     </div>
                   </th>
                   <th scope="col" className="px-4 py-3">
-                    <div className="flex items-center">Descripción (Otro)</div>
-                  </th>
-                  <th scope="col" className="px-4 py-3">
                     <div className="flex items-center">Acción</div>
                   </th>
                 </tr>
@@ -387,12 +486,11 @@ export function Curriculo() {
                     className="bg-gray-400 border-b dark:bg-gray-800 dark:border-gray-700"
                   >
                     <td className="px-4 py-4">{area.nombre}</td>
-                    <td className="px-4 py-4">{area.nombre_otro}</td>
                     <td className="px-4 py-4">
                       <a
                         href="#"
                         className="font-medium text-red-600 dark:text-blue-500"
-                        onClick={handleDeleteRow}
+                        onClick={handleDeleteArea}
                       >
                         Borrar
                       </a>
@@ -403,90 +501,102 @@ export function Curriculo() {
             </table>
           </div>
           <div>
-            <Label htmlFor="experiencia_tipo">
+            <Label htmlFor="tipo">
               ¿Posees experiencia laboral o realizaste algún curso?
             </Label>
-            <Select
-              id="experiencia_tipo"
-              name="experiencia_tipo"
-              value={datosCurriculo.experiencia_tipo}
-              onChange={handleInputChangeCurriculo}
-            >
+            <Select id="tipo" name="tipo" onChange={handleTipoExpSelected}>
               <option value="Ninguno">Ninguno</option>
               <option value="Laboral">Experiencia Laboral</option>
               <option value="Curso">Experiencia Curso</option>
             </Select>
           </div>
-          <div>
-            <Label htmlFor="experiencia_duracion">
-              Duración de la experiencia
+          <div className={` ${isHidden ? "hidden" : ""}`}>
+            <Label htmlFor="cargo_titulo">
+              Cargo laboral o título conseguido (Agregar todos uno por uno)
             </Label>
-            <Select
-              id="experiencia_duracion"
-              name="experiencia_duracion"
-              value={datosCurriculo.experiencia_duracion}
-              onChange={handleInputChangeCurriculo}
-            >
+
+            <Input
+              id="cargo_titulo"
+              type="text"
+              name="cargo_titulo"
+              placeholder="Ingrese el nombre del cargo o título"
+            />
+          </div>
+          <div className={` ${isHidden ? "hidden" : ""}`}>
+            <Label htmlFor="duracion">Duración de la experiencia</Label>
+            <Select id="duracion" name="duracion">
               <option value="Menos de 1 año">Menos de 1 año</option>
               <option value="1-2 años">1-2 años</option>
               <option value="3-4 años">3-4 años</option>
               <option value="5 años o más">5 años o más</option>
             </Select>
           </div>
-          <div>
-            <Label htmlFor="cargo_titulo_id">
-              ¿Cual fue tu cargo laboral o título obtenido en el curso?
+          <div className={` ${isHidden ? "hidden" : ""}`}>
+            <Label htmlFor="empresa_centro_educativo">
+              Nombre de la empresa / centro educativo
             </Label>
             <div className="flex gap-4 w-full items-start">
-              <Select
-                id="cargo_titulo_id"
-                name="cargo_titulo_id"
-                onChange={handleCargoTituloSelected}
-                className="inline-block"
-              >
-                {areas_interes &&
-                areas_interes.areas_interes &&
-                areas_interes.areas_interes.length > 0
-                  ? areas_interes?.areas_interes.map(
-                      (area, i) =>
-                        area.activo && (
-                          <option
-                            key={i}
-                            name={area.nombre}
-                            value={area.area_interes_id}
-                          >
-                            {area.nombre}
-                          </option>
-                        )
-                    )
-                  : null}
-              </Select>
-              <Button className="m-0">Agregar</Button>
+              <Input
+                id="empresa_centro_educativo"
+                type="text"
+                name="empresa_centro_educativo"
+                placeholder="Ingrese el nombre de la empresa / centro educativo"
+              />
+              <Button className="m-0" onClick={handleAddExperiencia}>
+                Agregar
+              </Button>
             </div>
           </div>
-          <div>
-            <Label htmlFor="cargo_titulo_otro">
-              Agregar otro cargo laboral o título obtenido en el curso
-              (Opcional)
-            </Label>
-            <Input
-              id="cargo_titulo_otro"
-              type="text"
-              name="cargo_titulo_otro"
-              placeholder="Ingrese el nombre del cargo o título"
-              readOnly={isReadOnly}
-            />
-          </div>
-          <div>
-            <Label htmlFor="empresa_centro_educativo">
-              Nombre de empresa / centro educativo
-            </Label>
-            <Input
-              id="empresa_centro_educativo"
-              type="text"
-              name="empresa_centro_educativo"
-              placeholder="Ingrese el nombre de la empresa / centro educativo"
-            />
+          <div className="md:col-span-3">
+            <table className="w-full mx-auto text-sm text-left rtl:text-right dark:text-gray-400">
+              <thead className="text-xs uppercase bg-blue-600 dark:bg-gray-700 dark:text-gray-400">
+                <tr className="text-white">
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex items-center">
+                      Experiencias agregadas
+                    </div>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex items-center">Cargo / Título</div>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex items-center">Duración</div>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex items-center">
+                      Empresa / Centro Educativo
+                    </div>
+                  </th>
+                  <th scope="col" className="px-4 py-3">
+                    <div className="flex items-center">Acción</div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {datosCurriculo.experiencias.map((experiencia, i) => (
+                  <tr
+                    key={i}
+                    className="bg-gray-400 border-b dark:bg-gray-800 dark:border-gray-700"
+                  >
+                    <td className="px-4 py-4">{experiencia.tipo}</td>
+                    <td className="px-4 py-4">{experiencia.cargo_titulo}</td>
+                    <td className="px-4 py-4">{experiencia.duracion}</td>
+                    <td className="px-4 py-4">
+                      {experiencia.empresa_centro_educativo}
+                    </td>
+                    <td className="px-4 py-4">
+                      <a
+                        href="#"
+                        className="font-medium text-red-600 dark:text-blue-500"
+                        onClick={handleDeleteExp}
+                      >
+                        Borrar
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
           <div className="flex items-end">
             <Button className="m-0" onClick={handleCreateCurriculo}>
