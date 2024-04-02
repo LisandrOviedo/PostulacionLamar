758bd0bc4e73518c6eef60e3b94729830850511c
lisandroviedo <lisandroviedo@hotmail.com>
2024-04-02 00:26:35 -0400

    Actualización del redux para implementar protección de rutas

--- Client/src/redux/empleados/empleadoAction.js
+++ Client/src/redux/empleados/empleadoAction.js
@@ -1,17 +1,37 @@
 import axios from "axios";
 
-import { empleado, cargoActual } from "./empleadoSlice";
+import {
+  allEmpleados,
+  createEmpleado,
+  empleadoByID,
+  cargoActualEmpleado,
+  resetState,
+} from "./empleadoSlice";
 
 const URL_SERVER = import.meta.env.VITE_URL_SERVER;
 
-export const getEmpleado = (empleado_id) => {
+export const getLogin = (cedula, clave) => {
+  const URL_LOGIN = `${URL_SERVER}/empleados/login?cedula=${cedula}&clave=${clave}`;
+
+  return async (dispatch) => {
+    try {
+      const { data } = await axios(URL_LOGIN);
+
+      return dispatch(createEmpleado(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
+export const getEmpleadoByID = (empleado_id) => {
   return async (dispatch) => {
     try {
       const { data } = await axios(
         `${URL_SERVER}/empleados/detalle/${empleado_id}`
       );
 
-      return dispatch(empleado(data));
+      return dispatch(empleadoByID(data));
     } catch (error) {
       console.error(error);
     }
@@ -25,7 +45,7 @@ export const getCargoActual = (empleado_id) => {
         `${URL_SERVER}/empleados/cargoActual/${empleado_id}`
       );
 
-      return dispatch(cargoActual(data));
+      return dispatch(cargoActualEmpleado(data));
     } catch (error) {
       console.error(error);
     }
