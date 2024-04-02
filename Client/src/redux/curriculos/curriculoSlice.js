f5e439a3c5c841dee5c6a34b60e098dd4f0c379d
LisandrOviedo <lisandroviedo@hotmail.com>
2024-04-02 16:24:07 -0400

    Nuevo estado global para obtener el curriculo de un empleado a través de su empleado_id

--- Client/src/redux/curriculos/curriculoSlice.js
+++ Client/src/redux/curriculos/curriculoSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   curriculos: [],
   curriculo: {},
+  curriculoEmpleado: {},
 };
 
 export const curriculoSlice = createSlice({
@@ -15,8 +16,12 @@ export const curriculoSlice = createSlice({
     createCurriculo: (state, action) => {
       state.curriculo = action.payload;
     },
+    curriculoEmpleado: (state, action) => {
+      state.curriculoEmpleado = action.payload;
+    },
   },
 });
 
-export const { allCurriculos, createCurriculo } = curriculoSlice.actions;
+export const { allCurriculos, createCurriculo, curriculoEmpleado } =
+  curriculoSlice.actions;
 export default curriculoSlice.reducer;
