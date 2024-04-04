21be1332f30e4ef2ad6ea65b5d9173dd8bf63155
LisandrOviedo <lisandroviedo@hotmail.com>
2024-04-04 15:13:46 -0400

    Preparación para actualizar currículo (en desarrollo)

--- Client/src/redux/curriculos/curriculoAction.js
+++ Client/src/redux/curriculos/curriculoAction.js
@@ -50,6 +50,8 @@ export const getCurriculoEmpleado = (empleado_id) => {
   };
 };
 
+// POST CURRICULO
+
 export const postCurriculo = (
   formData,
   areas_interes,
@@ -74,11 +76,11 @@ export const postCurriculo = (
       }
 
       return Swal.fire({
-        title: "¡Curriculo registrado exitosamente!",
+        title: "¡Currículo enviado exitosamente!",
         text: "Si deseas actualizar o ver los detalles de tu currículo, debes iniciar sesión nuevamente",
         icon: "success",
         showConfirmButton: false,
-        timer: 2000,
+        timer: 3000,
       });
     } catch (error) {
       Swal.fire({
@@ -159,6 +161,8 @@ const postExperiencias = async (curriculo_id, experiencias) => {
   }
 };
 
+// FIN CREAR CURRICULO
+
 export const resetCurriculos = () => {
   return async (dispatch) => {
     try {
@@ -175,3 +179,114 @@ export const resetCurriculos = () => {
     }
   };
 };
+
+// UPDATE CURRICULO
+
+export const putCurriculo = (
+  formData,
+  areas_interes,
+  titulos_obtenidos,
+  experiencias
+) => {
+  const URL_PUT_CURRICULO = `${URL_SERVER}/curriculos/modificar`;
+
+  return async () => {
+    try {
+      const { data } = await axios.put(`${URL_PUT_CURRICULO}`, formData);
+
+      await putAreasInteres(data.curriculo_id, areas_interes);
+
+      if (titulos_obtenidos) {
+        await putTitulosObtenidos(data.curriculo_id, titulos_obtenidos);
+      }
+
+      if (experiencias) {
+        await putExperiencias(data.curriculo_id, experiencias);
+      }
+
+      return Swal.fire({
+        title: "¡Curriculo actualizado exitosamente!",
+        icon: "success",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    } catch (error) {
+      Swal.fire({
+        title: "Oops...",
+        text: `${error.response.data.error}`,
+        icon: "error",
+        showConfirmButton: false,
+        timer: 2000,
+      });
+      throw new Error();
+    }
+  };
+};
+
+const putAreasInteres = async (curriculo_id, areas_interes) => {
+  const URL_ADD_AREASINTERES = `${URL_SERVER}/areasinteres/agregarArea`;
+
+  try {
+    const { data } = await axios.post(`${URL_ADD_AREASINTERES}`, {
+      curriculo_id,
+      areas_interes,
+    });
+
+    return data;
+  } catch (error) {
+    Swal.fire({
+      title: "Oops...",
+      text: `${error.response.data.error}`,
+      icon: "error",
+      showConfirmButton: false,
+      timer: 2000,
+    });
+    throw new Error();
+  }
+};
+
+const putTitulosObtenidos = async (curriculo_id, titulos_obtenidos) => {
+  const URL_ADD_TITULOSOBTENIDOS = `${URL_SERVER}/titulosobtenidos`;
+
+  try {
+    const { data } = await axios.post(`${URL_ADD_TITULOSOBTENIDOS}`, {
+      curriculo_id,
+      titulos_obtenidos,
+    });
+
+    return data;
+  } catch (error) {
+    Swal.fire({
+      title: "Oops...",
+      text: `${error.response.data.error}`,
+      icon: "error",
+      showConfirmButton: false,
+      timer: 2000,
+    });
+    throw new Error();
+  }
+};
+
+const putExperiencias = async (curriculo_id, experiencias) => {
+  const URL_ADD_EXPERIENCIAS = `${URL_SERVER}/experiencias`;
+
+  try {
+    const { data } = await axios.post(`${URL_ADD_EXPERIENCIAS}`, {
+      curriculo_id,
+      experiencias,
+    });
+
+    return data;
+  } catch (error) {
+    Swal.fire({
+      title: "Oops...",
+      text: `${error.response.data.error}`,
+      icon: "error",
+      showConfirmButton: false,
+      timer: 2000,
+    });
+    throw new Error();
+  }
+};
+
+// FIN PUT CURRICULO
