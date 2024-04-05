54a853af4525555daebf529fcff4cf774a255c52
LisandrOviedo <lisandroviedo@hotmail.com>
2024-04-05 11:08:00 -0400

    Habilitado acceso a los admin a la parte de empleados

--- Client/src/guards/AuthGuardEmpleado.jsx
+++ Client/src/guards/AuthGuardEmpleado.jsx
@@ -4,7 +4,8 @@ import { Navigate, Outlet } from "react-router-dom";
 export const AuthGuardEmpleado = () => {
   const userState = useSelector((state) => state.empleados.empleado);
 
-  return userState.empleado_id && userState.rol === "empleado" ? (
+  return (userState.empleado_id && userState.rol === "empleado") ||
+    userState.rol === "admin" ? (
     <Outlet />
   ) : (
     <Navigate replace to="/" />
