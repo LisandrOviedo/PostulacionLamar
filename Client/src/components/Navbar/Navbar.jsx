60b7ecd1ed25f3ac0a5825e035fa292969c2ded0
LisandrOviedo <lisandroviedo@hotmail.com>
2024-04-08 17:04:34 -0400

    Rediseño de navbar en relación a las últimas observaciones del cliente

--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -4,107 +4,214 @@ import { useState } from "react";
 import { Logo } from "../UI";
 
 export function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState({});
+  const [isOpenBurger, setIsOpenBurger] = useState(true);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
+  const toggleMenu = (index) => {
+    setIsOpen((prevState) => ({
+      ...prevState,
+      [index]: !prevState[index],
+    }));
+  };
+
+  const toggleMenuBurger = () => {
+    setIsOpenBurger(!isOpenBurger);
   };
 
   const { pathname } = useLocation();
 
   return (
-    <nav className="bg-[#002846] border-gray-200 w-full z-999 fixed top-0">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
-        <Link
-          to="https://grupo-lamar.com/es/"
-          target="_blank"
-          className="hover:opacity-70"
-        >
-          <Logo className="w-24" />
-        </Link>
-        <button
-          onClick={toggleMenu}
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden"
-          aria-controls="navbar-default"
-          aria-expanded={isOpen}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
+    <div className="w-full z-999 fixed top-0">
+      <nav className="bg-[#002846] p-1 flex items-center justify-between">
+        <div>
+          <Link
+            to="https://grupo-lamar.com/es/"
+            target="_blank"
+            className="hover:opacity-70"
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div
-          className={`w-full md:block md:w-auto ${isOpen ? "block" : "hidden"}`}
-          id="navbar-default"
+            <Logo className="w-24 ml-2" />
+          </Link>
+        </div>
+        <div className="flex items-center space-x-4">
+          <span className="text-white">Bienvenido</span>
+          <img src="/Person.svg" alt="Icono de Perfil" className="w-8" />
+        </div>
+        <span
+          onClick={toggleMenuBurger}
+          className="text-white cursor-pointer hover:opacity-70 mr-6"
         >
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+          Menú
+        </span>
+      </nav>
+
+      <aside
+        id="sidebar"
+        className={`bg-[#002846] text-white w-full sm:w-56 min-h-screen p-4 ${
+          isOpenBurger ? "block" : "hidden"
+        }`}
+      >
+        <img
+          src="/Close.svg"
+          alt="Icono de Perfil"
+          className="w-6 cursor-pointer hover:opacity-70 m-2 mx-auto"
+          onClick={toggleMenuBurger}
+        />
+        <nav>
+          <ul className="space-y-2 cursor-pointer">
             {!pathname.startsWith("/admin/") && (
+              // EMPLEADOS
               <>
+                <Link
+                  to="/home"
+                  className="block text-white hover:text-[#F0C95C]"
+                >
+                  <div className="flex items-center justify-between p-2 hover:bg-gray-700">
+                    <div className="mx-auto">Inicio</div>
+                  </div>
+                </Link>
+                <li>
+                  <div
+                    onClick={() => toggleMenu(0)}
+                    data-index={0}
+                    className="flex items-center justify-between p-2 hover:bg-gray-700 hover:text-[#F0C95C]"
+                  >
+                    <div className="mx-auto">
+                      <span>Mi Perfil</span>
+                    </div>
+                  </div>
+                  <ul className={isOpen[0] ? "flex flex-col gap-2" : "hidden"}>
+                    <li>
+                      <Link
+                        to="/home"
+                        className="block text-white hover:text-[#F0C95C] text-sm text-center hover:bg-gray-700"
+                      >
+                        Datos personales
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/home"
+                        className="block text-white hover:text-[#F0C95C] text-sm text-center hover:bg-gray-700"
+                      >
+                        Solicitar reinicio de contraseña
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <div
+                    onClick={() => toggleMenu(1)}
+                    data-index={1}
+                    className="flex items-center justify-between p-2 hover:bg-gray-700 hover:text-[#F0C95C]"
+                  >
+                    <div className="mx-auto">
+                      <span>Currículo</span>
+                    </div>
+                  </div>
+                  <ul className={isOpen[1] ? "flex flex-col gap-2" : "hidden"}>
+                    <li>
+                      <Link
+                        to="/home"
+                        className="block text-white hover:text-[#F0C95C] text-sm text-center hover:bg-gray-700"
+                      >
+                        Visualizar
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/home"
+                        className="block text-white hover:text-[#F0C95C] text-sm text-center hover:bg-gray-700"
+                      >
+                        Crear / Modificar
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/home"
+                        className="block text-white hover:text-[#F0C95C] text-sm text-center hover:bg-gray-700"
+                      >
+                        Anexar documentos
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
                 <li>
                   <Link
                     to="/"
-                    className="block py-2 px-3 text-white hover:text-[#F0C95C]"
+                    className="block text-white hover:text-[#F0C95C]"
                   >
-                    Inicio
+                    <div className="flex items-center justify-between p-2 hover:bg-gray-700">
+                      <div className="mx-auto">Cerrar Sesión</div>
+                    </div>
                   </Link>
                 </li>
               </>
             )}
-            {pathname === "/admin/login" && (
+            {pathname.startsWith("/admin/") && (
+              // ADMINISTRADORES
               <>
+                <Link
+                  to="/admin/dashboard"
+                  className="block text-white hover:text-[#F0C95C]"
+                >
+                  <div className="flex items-center justify-between p-2 hover:bg-gray-700">
+                    <div className="mx-auto">Inicio</div>
+                  </div>
+                </Link>
                 <li>
-                  <Link
-                    to="/"
-                    className="block py-2 px-3 text-white hover:text-[#F0C95C]"
+                  <div
+                    onClick={() => toggleMenu(0)}
+                    data-index={0}
+                    className="flex items-center justify-between p-2 hover:bg-gray-700 hover:text-[#F0C95C]"
                   >
-                    Ir a vista de empleado
-                  </Link>
-                </li>
-              </>
-            )}
-            {pathname.startsWith("/admin/") && pathname !== "/admin/login" && (
-              <>
-                <li>
-                  <Link
-                    to="/admin/dashboard"
-                    className="block py-2 px-3 text-white hover:text-[#F0C95C]"
-                  >
-                    Inicio
-                  </Link>
+                    <div className="mx-auto">
+                      <span>Mi Perfil</span>
+                    </div>
+                  </div>
+                  <ul className={isOpen[0] ? "flex flex-col gap-2" : "hidden"}>
+                    <li>
+                      <Link
+                        to="/admin/dashboard"
+                        className="block text-white hover:text-[#F0C95C] text-sm text-center hover:bg-gray-700"
+                      >
+                        Datos personales
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/admin/dashboard"
+                        className="block text-white hover:text-[#F0C95C] text-sm text-center hover:bg-gray-700"
+                      >
+                        Solicitar reinicio de contraseña
+                      </Link>
+                    </li>
+                  </ul>
                 </li>
                 <li>
                   <Link
                     to="/admin/postulaciones"
-                    className="block py-2 px-3 text-white hover:text-[#F0C95C]"
+                    className="block text-white hover:text-[#F0C95C]"
                   >
-                    Postulaciones
+                    <div className="flex items-center justify-between p-2 hover:bg-gray-700">
+                      <div className="mx-auto">Postulaciones</div>
+                    </div>
                   </Link>
                 </li>
                 <li>
                   <Link
                     to="/admin/login"
-                    className="block py-2 px-3 text-white hover:text-[#F0C95C]"
+                    className="block text-white hover:text-[#F0C95C]"
                   >
-                    Cerrar Sesión
+                    <div className="flex items-center justify-between p-2 hover:bg-gray-700">
+                      <div className="mx-auto">Cerrar Sesión</div>
+                    </div>
                   </Link>
                 </li>
               </>
             )}
           </ul>
-        </div>
-      </div>
-    </nav>
+        </nav>
+      </aside>
+    </div>
   );
 }
