a953ba2c439e4b91f17c616e3e96ec4b4ac15d93
LisandrOviedo <lisandroviedo@hotmail.com>
2024-04-11 14:02:50 -0400

    Añadidas validaciones Botón de Enviar Documentos ya registra correctamente los documentos del empleado Realizado algunos ajustes visuales

--- Client/src/components/AnexarDocumentos/AnexarDocumentos.jsx
+++ Client/src/components/AnexarDocumentos/AnexarDocumentos.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useSelector, useDispatch } from "react-redux";
 
 import { Button, Label, Title, InputFile } from "../UI";
+
+import { postDocumentos } from "../../redux/empleados/empleadoAction";
 
 import Swal from "sweetalert2";
 
@@ -14,6 +16,18 @@ export function AnexarDocumentos() {
   const navigate = useNavigate();
 
   const empleado = useSelector((state) => state.empleados.empleado);
+
+  const [isLoad, setIsLoad] = useState({
+    foto_carnet: false,
+    foto_cedula: false,
+    rif: false,
+    resumen_curricular: false,
+    titulo_bachiller: false,
+    titulos_universitarios: false,
+    otros_estudios: false,
+    referencia_personal: false,
+    cuenta_bancaria: false,
+  });
 
   useEffect(() => {
     window.scroll(0, 0);
@@ -28,8 +42,10 @@ export function AnexarDocumentos() {
   const handleValidatePDF = (event) => {
     const input = event.target;
     const file = input.files[0];
+    const { name } = event.target;
 
     if (!file) {
+      setIsLoad({ ...isLoad, [name]: false });
       return;
     }
 
@@ -37,6 +53,8 @@ export function AnexarDocumentos() {
 
     if (!allowedTypes.includes(file.type)) {
       input.value = "";
+      setIsLoad({ ...isLoad, [name]: false });
+
       Swal.fire({
         title: "Oops...",
         text: "¡Solo se permiten archivos PDF!",
@@ -46,13 +64,17 @@ export function AnexarDocumentos() {
       });
       return;
     }
+
+    setIsLoad({ ...isLoad, [name]: true });
   };
 
   const handleValidateImage = (event) => {
     const input = event.target;
     const file = input.files[0];
+    const { name } = event.target;
 
     if (!file) {
+      setIsLoad({ ...isLoad, [name]: false });
       return;
     }
 
@@ -60,6 +82,8 @@ export function AnexarDocumentos() {
 
     if (!allowedTypes.includes(file.type)) {
       input.value = "";
+      setIsLoad({ ...isLoad, [name]: false });
+
       Swal.fire({
         title: "Oops...",
         text: "¡Solo se permiten archivos JPG / JPEG / PNG!",
@@ -69,13 +93,17 @@ export function AnexarDocumentos() {
       });
       return;
     }
+
+    setIsLoad({ ...isLoad, [name]: true });
   };
 
   const handleValidateImagePDF = (event) => {
     const input = event.target;
     const file = input.files[0];
+    const { name } = event.target;
 
     if (!file) {
+      setIsLoad({ ...isLoad, [name]: false });
       return;
     }
 
@@ -88,6 +116,8 @@ export function AnexarDocumentos() {
 
     if (!allowedTypes.includes(file.type)) {
       input.value = "";
+      setIsLoad({ ...isLoad, [name]: false });
+
       Swal.fire({
         title: "Oops...",
         text: "¡Solo se permiten archivos JPG / JPEG / PNG / PDF!",
@@ -97,55 +127,104 @@ export function AnexarDocumentos() {
       });
       return;
     }
+
+    setIsLoad({ ...isLoad, [name]: true });
   };
 
   const handleCreateAnexos = async (event) => {
     event.preventDefault();
 
-    // const input = document.getElementById("pdf");
-    // const pdf = input.files[0];
+    const foto_carnet = document.getElementById("foto_carnet");
+    const foto_carnet_file = foto_carnet.files[0];
 
-    // if (
-    //   !input.value ||
-    //   !datosCurriculo.grado_instruccion ||
-    //   !datosCurriculo.areas_interes.length
-    // ) {
-    //   return Swal.fire({
-    //     title: "Oops...",
-    //     text: "Datos faltantes",
-    //     icon: "error",
-    //     showConfirmButton: false,
-    //     timer: 1500,
-    //   });
-    // }
+    const foto_cedula = document.getElementById("foto_cedula");
+    const foto_cedula_file = foto_cedula.files[0];
 
-    // const formData = new FormData();
-    // formData.append("pdf", pdf);
-    // formData.append("cedula", empleado.cedula);
-    // formData.append("empleado_id", empleado.empleado_id);
-    // formData.append("grado_instruccion", datosCurriculo.grado_instruccion);
-    // formData.append("centro_educativo", datosCurriculo.centro_educativo);
-    // formData.append(
-    //   "disponibilidad_viajar",
-    //   datosCurriculo.disponibilidad_viajar
-    // );
-    // formData.append(
-    //   "disponibilidad_cambio_residencia",
-    //   datosCurriculo.disponibilidad_cambio_residencia
-    // );
+    const rif = document.getElementById("rif");
+    const rif_file = rif.files[0];
 
-    // try {
-    //   dispatch(
-    //     postCurriculo(
-    //       formData,
-    //       datosCurriculo.areas_interes,
-    //       datosCurriculo.titulos_obtenidos,
-    //       datosCurriculo.experiencias
-    //     )
-    //   );
-    // } catch (error) {
-    //   return error;
-    // }
+    const resumen_curricular = document.getElementById("resumen_curricular");
+    const resumen_curricular_file = resumen_curricular.files[0];
+
+    const titulo_bachiller = document.getElementById("titulo_bachiller");
+    const titulo_bachiller_file = titulo_bachiller.files[0];
+
+    const titulos_universitarios = document.getElementById(
+      "titulos_universitarios"
+    );
+    const titulos_universitarios_file = titulos_universitarios.files[0];
+
+    const otros_estudios = document.getElementById("otros_estudios");
+    const otros_estudios_file = otros_estudios.files[0];
+
+    const referencia_personal = document.getElementById("referencia_personal");
+    const referencia_personal_file = referencia_personal.files[0];
+
+    const cuenta_bancaria = document.getElementById("cuenta_bancaria");
+    const cuenta_bancaria_file = cuenta_bancaria.files[0];
+
+    if (
+      foto_carnet.value ||
+      foto_cedula.value ||
+      rif.value ||
+      resumen_curricular.value ||
+      titulo_bachiller.value ||
+      titulos_universitarios.value ||
+      otros_estudios.value ||
+      referencia_personal.value ||
+      cuenta_bancaria.value
+    ) {
+      const formData = new FormData();
+      formData.append("cedula", empleado.cedula);
+
+      if (foto_carnet.value) {
+        formData.append("anexos", foto_carnet_file);
+      }
+
+      if (foto_cedula.value) {
+        formData.append("anexos", foto_cedula_file);
+      }
+
+      if (rif.value) {
+        formData.append("anexos", rif_file);
+      }
+
+      if (resumen_curricular.value) {
+        formData.append("anexos", resumen_curricular_file);
+      }
+
+      if (titulo_bachiller.value) {
+        formData.append("anexos", titulo_bachiller_file);
+      }
+
+      if (titulos_universitarios.value) {
+        formData.append("anexos", titulos_universitarios_file);
+      }
+
+      if (otros_estudios.value) {
+        formData.append("anexos", otros_estudios_file);
+      }
+      if (referencia_personal.value) {
+        formData.append("anexos", referencia_personal_file);
+      }
+      if (cuenta_bancaria.value) {
+        formData.append("anexos", cuenta_bancaria_file);
+      }
+
+      try {
+        dispatch(postDocumentos(formData));
+      } catch (error) {
+        return error;
+      }
+    }
+
+    Swal.fire({
+      title: "Oops...",
+      text: "¡Debes cargar al menos 1 archivo!",
+      icon: "error",
+      showConfirmButton: false,
+      timer: 2500,
+    });
   };
 
   return (
@@ -153,7 +232,7 @@ export function AnexarDocumentos() {
       <Title>Mis documentos</Title>
       <hr className="w-[80%] h-0.5 my-5 bg-gray-300 border-0 m-auto" />
 
-      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 mt-5 mb-5">
+      <div className="grid gap-10 grid-cols-1 md:grid-cols-2 mt-5 mb-5">
         <div className="flex flex-col place-content-between">
           <Label htmlFor="foto_carnet">
             Adjunte una foto tipo carnet{" "}
@@ -166,20 +245,26 @@ export function AnexarDocumentos() {
             name="foto_carnet"
             accept=".jpg, .jpeg, .png"
             onChange={handleValidateImage}
+            className={`${
+              isLoad.foto_carnet ? "border-green-500" : "border-red-500"
+            }`}
           />
         </div>
         <div className="flex flex-col place-content-between">
-          <Label htmlFor="cedula">
+          <Label htmlFor="foto_cedula">
             Adjunte su cédula de identidad{" "}
             <span className="mt-1 text-sm text-red-600 dark:text-gray-300">
               (JPG, JPEG, PNG, PDF)
             </span>
           </Label>
           <InputFile
-            id="cedula"
-            name="cedula"
+            id="foto_cedula"
+            name="foto_cedula"
             accept=".jpg, .jpeg, .png, application/pdf"
             onChange={handleValidateImagePDF}
+            className={`${
+              isLoad.foto_cedula ? "border-green-500" : "border-red-500"
+            }`}
           />
         </div>
         <div className="flex flex-col place-content-between">
@@ -194,6 +279,7 @@ export function AnexarDocumentos() {
             name="rif"
             accept="application/pdf"
             onChange={handleValidatePDF}
+            className={`${isLoad.rif ? "border-green-500" : "border-red-500"}`}
           />
         </div>
         <div className="flex flex-col place-content-between">
@@ -208,6 +294,9 @@ export function AnexarDocumentos() {
             name="resumen_curricular"
             accept="application/pdf"
             onChange={handleValidatePDF}
+            className={`${
+              isLoad.resumen_curricular ? "border-green-500" : "border-red-500"
+            }`}
           />
         </div>
         <div className="flex flex-col place-content-between">
@@ -222,6 +311,9 @@ export function AnexarDocumentos() {
             name="titulo_bachiller"
             accept=".jpg, .jpeg, .png, application/pdf"
             onChange={handleValidateImagePDF}
+            className={`${
+              isLoad.titulo_bachiller ? "border-green-500" : "border-red-500"
+            }`}
           />
         </div>
         <div className="flex flex-col place-content-between">
@@ -236,6 +328,11 @@ export function AnexarDocumentos() {
             name="titulos_universitarios"
             accept="application/pdf"
             onChange={handleValidatePDF}
+            className={`${
+              isLoad.titulos_universitarios
+                ? "border-green-500"
+                : "border-red-500"
+            }`}
           />
         </div>
         <div className="flex flex-col place-content-between">
@@ -250,6 +347,9 @@ export function AnexarDocumentos() {
             name="otros_estudios"
             accept="application/pdf"
             onChange={handleValidatePDF}
+            className={`${
+              isLoad.otros_estudios ? "border-green-500" : "border-red-500"
+            }`}
           />
         </div>
         <div className="flex flex-col place-content-between">
@@ -264,6 +364,9 @@ export function AnexarDocumentos() {
             name="referencia_personal"
             accept="application/pdf"
             onChange={handleValidatePDF}
+            className={`${
+              isLoad.referencia_personal ? "border-green-500" : "border-red-500"
+            }`}
           />
         </div>
         <div className="flex flex-col place-content-between">
@@ -278,6 +381,9 @@ export function AnexarDocumentos() {
             name="cuenta_bancaria"
             accept=".jpg, .jpeg, .png, application/pdf"
             onChange={handleValidateImagePDF}
+            className={`${
+              isLoad.cuenta_bancaria ? "border-green-500" : "border-red-500"
+            }`}
           />
         </div>
         <div className="md:col-span-2 flex justify-center items-center">
