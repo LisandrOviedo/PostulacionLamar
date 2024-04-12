93f49e9fd44a232cafd665dcae8c799e59d008c3
LisandrOviedo <lisandroviedo@hotmail.com>
2024-04-12 09:56:30 -0400

    Corrección de campos enum para el correcto guardado de tipo de documentos

--- Server/src/models/Documentos_Empleado.js
+++ Server/src/models/Documentos_Empleado.js
@@ -15,17 +15,17 @@ module.exports = (sequelize) => {
     },
     tipo_documento: {
       type: DataTypes.ENUM(
-        "Foto Carnet",
-        "Cedula",
-        "RIF",
-        "Curriculo",
-        "Titulo Bachiller",
-        "Titulo Universitario",
-        "Otros Estudios",
-        "Referencia Personal",
-        "Soporte Cuenta Bancaria"
+        "foto_carnet",
+        "foto_cedula",
+        "rif",
+        "resumen_curricular",
+        "titulo_bachiller",
+        "titulos_universitarios",
+        "otros_estudios",
+        "referencia_personal",
+        "cuenta_bancaria"
       ),
-      allowNull: false,
+      allowNull: true,
     },
     ruta_documento: {
       type: DataTypes.STRING,
