cac5915abf842394580603d1bd140c79ad5e3f47
LisandrOviedo <lisandroviedo@hotmail.com>
2024-05-10 17:01:53 -0400

    Añadida validación para capturar cuando el error es por No-Autorización de JWT

--- Client/src/utils/sweetAlert2.js
+++ Client/src/utils/sweetAlert2.js
@@ -1,6 +1,18 @@
 import Swal from "sweetalert2";
 
-export const alertError = (error) => {
+export const alertError = async (error) => {
+  if (error.response.status === 403) {
+    await Swal.fire({
+      title: "Oops...",
+      text: "Debes iniciar sesión nuevamente",
+      icon: "error",
+      showConfirmButton: false,
+      timer: 2000,
+    });
+
+    window.location.href = "/";
+  }
+
   Swal.fire({
     title: "Oops...",
     text: `${error.response.data.error}`,
