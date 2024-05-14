552fe5374c4ca2fd79c7c563126501e386c4af8d
LisandrOviedo <lisandroviedo@hotmail.com>
2024-05-14 11:09:58 -0400

    Añadidas respuestas faltantes

--- Server/src/utils/pruebaKostick.js
+++ Server/src/utils/pruebaKostick.js
@@ -1,4 +1,4 @@
-const arreglo = [
+const pruebaKostick = [
   { pregunta: 1, respuesta: "a. Soy trabajador tenaz" },
   { pregunta: 1, respuesta: "b. No soy voluble" },
   {
@@ -93,112 +93,402 @@ const arreglo = [
   },
   {
     pregunta: 20,
-    respuesta: "b. Me gusta experimentar trabajos nuevos y diferentes",
+    respuesta: "b. Me gustaría decírselo a la gente cuando me molestan",
   },
   {
     pregunta: 21,
-    respuesta: "a. Me gusta tener una amplia gama de experiencias",
+    respuesta: "a. Siempre trabajo intensamente",
   },
   {
     pregunta: 21,
-    respuesta: "b. Me gusta tener una estrecha relación con unos pocos amigos",
+    respuesta: "b. Me gusta hacer las cosas una por una detalladamente",
   },
-  { pregunta: 22, respuesta: "a. Me gusta que me digan qué hacer" },
-  { pregunta: 22, respuesta: "b. Me gusta tomar mis propias decisiones" },
-  { pregunta: 23, respuesta: "a. Me gusta ser el centro de atención" },
-  { pregunta: 23, respuesta: "b. Me gusta ser una persona reservada" },
-  { pregunta: 24, respuesta: "a. Me gusta buscar aventuras" },
-  { pregunta: 24, respuesta: "b. Me gusta mantenerme seguro y protegido" },
-  { pregunta: 25, respuesta: "a. Me gusta tener una rutina establecida" },
-  { pregunta: 25, respuesta: "b. Me gusta probar cosas nuevas y espontáneas" },
-  { pregunta: 26, respuesta: "a. Me gusta que me valoren por mi trabajo" },
-  { pregunta: 26, respuesta: "b. Me gusta que me valoren por mi personalidad" },
-  { pregunta: 27, respuesta: "a. Me gusta trabajar en equipo" },
-  { pregunta: 27, respuesta: "b. Me gusta trabajar de forma independiente" },
+  { pregunta: 22, respuesta: "a. Soy un buen dirigente" },
+  { pregunta: 22, respuesta: "b. Yo organizo muy bien mi trabajo" },
+  { pregunta: 23, respuesta: "a. Me enfado con facilidad" },
+  { pregunta: 23, respuesta: "b. Soy lento para la toma de decisiones" },
+  {
+    pregunta: 24,
+    respuesta: "a. Me gusta trabajar en varias actividades al mismo tiempo",
+  },
+  {
+    pregunta: 24,
+    respuesta: "b. Cuando estoy en un grupo, me gusta estar callado",
+  },
+  { pregunta: 25, respuesta: "a. Me gusta que me inviten" },
+  {
+    pregunta: 25,
+    respuesta: "b. Me gusta hacer las cosas mejor que los demás",
+  },
+  { pregunta: 26, respuesta: "a. Me gusta tener amigos íntimos" },
+  { pregunta: 26, respuesta: "b. Me gusta aconsejar a los demás" },
+  { pregunta: 27, respuesta: "a. Me gusta hacer cosas nuevas y diferentes" },
+  {
+    pregunta: 27,
+    respuesta: "b. Me gusta hablar de la forma como obtengo mis éxitos",
+  },
   {
     pregunta: 28,
-    respuesta: "a. Me gusta tener el control de las situaciones",
+    respuesta: "a. Cuando tengo razón me gusta luchar por ella",
   },
   {
     pregunta: 28,
-    respuesta: "b. Me gusta seguir el flujo y adaptarme a las circunstancias",
+    respuesta: "b. Me gusta pertenecer a un grupo",
   },
-  { pregunta: 29, respuesta: "a. Me gusta recibir reconocimiento y elogios" },
-  { pregunta: 29, respuesta: "b. Me gusta mantener un perfil bajo" },
+  { pregunta: 29, respuesta: "a. Evito ser diferente a los demás" },
+  { pregunta: 29, respuesta: "b. Intento acercarme mucho a la gente" },
   {
     pregunta: 30,
-    respuesta:
-      "a. Me gusta explorar diferentes opciones antes de tomar una decisión",
+    respuesta: "a. Me gusta que me digan exactamente cómo hacer mi trabajo",
   },
   {
     pregunta: 30,
-    respuesta: "b. Me gusta tomar decisiones rápidas y seguir adelante",
+    respuesta: "b. Me aburro fácilmente",
   },
-  { pregunta: 31, respuesta: "a. Me gusta tener un horario estructurado" },
+  { pregunta: 31, respuesta: "a. Trabajo intensamente" },
   {
     pregunta: 31,
-    respuesta: "b. Me gusta tener flexibilidad y libertad en mi horario",
+    respuesta: "b. Pienso y planeo mucho",
   },
   {
     pregunta: 32,
-    respuesta: "a. Me gusta recibir instrucciones claras y específicas",
+    respuesta: "a. Yo dirijo al grupo",
   },
   {
     pregunta: 32,
-    respuesta: "b. Me gusta tener libertad para hacer las cosas a mi manera",
+    respuesta: "b. Los pequeños detalles me interesan",
   },
-  { pregunta: 33, respuesta: "a. Me gusta ser el líder en un grupo" },
-  { pregunta: 33, respuesta: "b. Me gusta contribuir como miembro del equipo" },
-  { pregunta: 34, respuesta: "a. Me gusta ser elogiado en público" },
-  { pregunta: 34, respuesta: "b. Me gusta recibir reconocimiento en privado" },
+  { pregunta: 33, respuesta: "a. Tomo decisiones fácil y rápidamente" },
+  { pregunta: 33, respuesta: "b. Tengo mis cosas limpias y ordenadas" },
+  { pregunta: 34, respuesta: "a. Hago las cosas con rapidez" },
+  { pregunta: 34, respuesta: "b. Yo casi nunca me enojo ni me pongo triste" },
   {
     pregunta: 35,
-    respuesta: "a. Me gusta explorar nuevas ideas y posibilidades",
+    respuesta: "a. Me gusta ser parte del grupo",
   },
   {
     pregunta: 35,
-    respuesta: "b. Me gusta seguir métodos y procedimientos establecidos",
+    respuesta: "b. Me gusta hacer un solo trabajo a la vez",
   },
-  { pregunta: 36, respuesta: "a. Me gusta tener un plan claro y seguirlo" },
+  { pregunta: 36, respuesta: "a. Intento hacer amigos íntimos" },
   {
     pregunta: 36,
-    respuesta: "b. Me gusta ir con la corriente y adaptarme a los cambios",
+    respuesta: "b. Yo pongo empeño en ser el mejor",
   },
   {
     pregunta: 37,
-    respuesta: "a. Me gusta ser el centro de atención en eventos sociales",
+    respuesta: "a. Me gustan los nuevos modelos en trajes y coches",
   },
   {
     pregunta: 37,
-    respuesta: "b. Me gusta pasar desapercibido en eventos sociales",
+    respuesta: "b. Me gusta ser responsable por otros",
   },
   {
     pregunta: 38,
-    respuesta: "a. Me gusta asumir riesgos y enfrentar desafíos",
+    respuesta: "a. Me gusta discutir",
   },
   {
     pregunta: 38,
-    respuesta: "b. Me gusta evitar situaciones arriesgadas y mantenerme seguro",
+    respuesta: "b. Me gusta que me pongan atención",
   },
   {
     pregunta: 39,
-    respuesta: "a. Me gusta tener una rutina estable y predecible",
+    respuesta: "a. Me gusta agradar a mis superiores",
   },
   {
     pregunta: 39,
-    respuesta: "b. Me gusta tener variedad y cambios en mi rutina",
+    respuesta: "b. Estoy interesado en ser parte del grupo",
   },
-  { pregunta: 40, respuesta: "a. Me gusta que me reconozcan por mis logros" },
+  { pregunta: 40, respuesta: "a. Me gusta apegarme a las reglas establecidas" },
   {
     pregunta: 40,
-    respuesta: "b. Me gusta que me aprecien por mi personalidad",
+    respuesta: "b. Me gusta que la gente me conozca muy bien",
   },
-  { pregunta: 41, respuesta: "a. Me gusta trabajar en colaboración con otros" },
-  { pregunta: 41, respuesta: "b. Me gusta trabajar de forma independiente" },
-  { pregunta: 42, respuesta: "a. Me gusta tomar decisiones y liderar" },
-  { pregunta: 42, respuesta: "b. Me gusta seguir las decisiones de otros" },
+  { pregunta: 41, respuesta: "a. Me esfuerzo mucho" },
+  { pregunta: 41, respuesta: "b. Soy muy amigable" },
+  { pregunta: 42, respuesta: "a. La gente piensa que soy un buen 'dirigente'" },
+  {
+    pregunta: 42,
+    respuesta: "b. Pienso las cosas con cuidado y detenidamente",
+  },
   {
     pregunta: 43,
-    respuesta: "a. Me gusta recibir elogios y reconocimiento público",
+    respuesta: "a. A menudo me arriesgo",
+  },
+  {
+    pregunta: 43,
+    respuesta: "a. Me gusta darle importancia a las cosas joviales",
+  },
+  {
+    pregunta: 44,
+    respuesta: "a. La gente piensa que trabajo con rapidez",
+  },
+  {
+    pregunta: 44,
+    respuesta: "b. La gente piensa que tengo mis cosas limpias y ordenadas",
+  },
+  {
+    pregunta: 45,
+    respuesta: "a. Me gusta jugar y hacer deportes",
+  },
+  {
+    pregunta: 45,
+    respuesta: "b. Soy muy agradable",
+  },
+  {
+    pregunta: 46,
+    respuesta: "a. Me gusta que la gente sea unida y sea amistosa",
+  },
+  {
+    pregunta: 46,
+    respuesta: "b. Siempre trato de terminar un trabajo difícil",
+  },
+  {
+    pregunta: 47,
+    respuesta: "a. Me gusta experimentar y probar cosas nuevas",
+  },
+  {
+    pregunta: 47,
+    respuesta: "b. Me gusta hacer bien un trabajo difícil",
+  },
+  {
+    pregunta: 48,
+    respuesta: "a. Me gusta que me traten justamente",
+  },
+  {
+    pregunta: 48,
+    respuesta: "b. Me gusta decir a los demás cómo hacer las cosas",
+  },
+  {
+    pregunta: 49,
+    respuesta: "a. Me gusta hacer aquello que esperan de mí",
+  },
+  {
+    pregunta: 49,
+    respuesta: "b. Me gusta que me pongan atención",
+  },
+  {
+    pregunta: 50,
+    respuesta: "a. Me gusta tener instrucciones precisas para hacer un trabajo",
+  },
+  {
+    pregunta: 50,
+    respuesta: "b. Me gusta estar con la gente",
+  },
+  { pregunta: 51, respuesta: "a. Siempre trato de hacer mi trabajo perfecto" },
+  { pregunta: 51, respuesta: "b. Me dicen que soy prácticamente incansable" },
+  {
+    pregunta: 52,
+    respuesta: "Soy el tipo 'dirigente'",
+  },
+  {
+    pregunta: 52,
+    respuesta: "b. Hago amigos fácilmente",
+  },
+  {
+    pregunta: 53,
+    respuesta: "a. Asumo riesgos",
+  },
+  { pregunta: 53, respuesta: "b. Pienso mucho" },
+  { pregunta: 54, respuesta: "a. Trabajo a un paso rápido y constante" },
+  {
+    pregunta: 54,
+    respuesta: "b. Disfruto trabajando con detalles",
+  },
+  { pregunta: 55, respuesta: "a. Tengo mucha energía para juegos y deportes" },
+  { pregunta: 55, respuesta: "b. Tengo mis cosas limpias y ordenadas" },
+  { pregunta: 56, respuesta: "a. Me llevo bien con todo el mundo" },
+  { pregunta: 56, respuesta: "b. Soy de temperamento estable" },
+  {
+    pregunta: 57,
+    respuesta: "a. Quiero conocer nueva gente y hacer cosas nuevas",
+  },
+  {
+    pregunta: 57,
+    respuesta: "b. Siempre quiero terminar el trabajo que he empezado",
+  },
+  {
+    pregunta: 58,
+    respuesta: "a. Normalmente sostengo mis creencias",
+  },
+  { pregunta: 58, respuesta: "b. Normalmente me gusta trabajar intensamente" },
+  {
+    pregunta: 59,
+    respuesta: "a. Me gustan las sugerencias de las personas que admiro",
+  },
+  {
+    pregunta: 59,
+    respuesta: "b. Me gusta tener a mi cargo a otras personas",
+  },
+  {
+    pregunta: 60,
+    respuesta: "a. Me dejo influenciar fácilmente por otras personas",
+  },
+  { pregunta: 60, respuesta: "b. Me gusta que me pongan mucha atención" },
+  { pregunta: 61, respuesta: "a. Normalmente trabajo intensamente" },
+  { pregunta: 61, respuesta: "b. Normalmente trabajo con rapidez" },
+  {
+    pregunta: 62,
+    respuesta: "a. Cuando hablo, el grupo me escucha",
+  },
+  { pregunta: 62, respuesta: "b. Soy hábil con herramientas" },
+  {
+    pregunta: 63,
+    respuesta: "a. Soy lento en hacer amigos",
+  },
+  {
+    pregunta: 63,
+    respuesta: "b. Me toma tiempo para llegar a una conclusión",
+  },
+  { pregunta: 64, respuesta: "a. Normalemente como de prisa" },
+  { pregunta: 64, respuesta: "b. Me gusta leer" },
+  { pregunta: 65, respuesta: "a. Me gusta el trabajo en donde puedo moverme" },
+  {
+    pregunta: 65,
+    respuesta: "b. Me gusta el trabajo que tiene que hacerse con cuidado",
+  },
+  { pregunta: 66, respuesta: "a. Hago el mayor número posible de amigos" },
+  { pregunta: 66, respuesta: "b. Encuentro lo que he guardado" },
+  { pregunta: 67, respuesta: "a. Pienso las cosas con anticipación" },
+  {
+    pregunta: 67,
+    respuesta: "b. Siempre soy agradable",
+  },
+  {
+    pregunta: 68,
+    respuesta: "a. Yo cuido mucho mi reputación",
+  },
+  {
+    pregunta: 68,
+    respuesta: "b. Yo le pongo atención a un problema hasta que se resuelve",
+  },
+  { pregunta: 69, respuesta: "a. Me gusta agradar a la gente que admiro" },
+  {
+    pregunta: 69,
+    respuesta: "b. Quiero tener éxito",
+  },
+  {
+    pregunta: 70,
+    respuesta: "a. Me gusta que otros tomen decisiones para el grupo",
+  },
+  {
+    pregunta: 70,
+    respuesta: "b. A  mi me gusta tomar decisiones para el grupo",
+  },
+  {
+    pregunta: 71,
+    respuesta: "a. Siempre me esfuerzo mucho",
+  },
+  {
+    pregunta: 71,
+    respuesta: "b. Tomo decisiones fácil y rápidamente",
+  },
+  { pregunta: 72, respuesta: "a. El grupo hace normalmente lo que yo quiero" },
+  { pregunta: 72, respuesta: "b. Normalmente tengo prisa" },
+  { pregunta: 73, respuesta: "a. A menudo me siento cansado" },
+  { pregunta: 73, respuesta: "b. Soy lento para tomar deisiones" },
+  {
+    pregunta: 74,
+    respuesta: "a. Trabajo con rapidez",
+  },
+  {
+    pregunta: 74,
+    respuesta: "b. Hago amigos fácilmente",
+  },
+  { pregunta: 75, respuesta: "a. Soy activo con mucho vigor" },
+  {
+    pregunta: 75,
+    respuesta: "b. Dedico mucho tiempo a pensar",
+  },
+  { pregunta: 76, respuesta: "a. Soy cordial con la gente" },
+  { pregunta: 76, respuesta: "b. Me gusta el trabajo que requiere precisión" },
+  { pregunta: 77, respuesta: "a. Pienso y planeo mucho" },
+  {
+    pregunta: 77,
+    respuesta: "b. Guardo cada cosa en su lugar",
+  },
+  {
+    pregunta: 78,
+    respuesta: "a. Me gusta el trabajo que requiere detalles",
+  },
+  {
+    pregunta: 78,
+    respuesta: "b. Siempre termino el trabajo que he empezado",
+  },
+  { pregunta: 79, respuesta: "a. Me gusta imitar a la gente que admiro" },
+  { pregunta: 79, respuesta: "b. Difícilmente me enojo" },
+  {
+    pregunta: 80,
+    respuesta: "a. Me gustan las instrucciones claras",
+  },
+  {
+    pregunta: 80,
+    respuesta: "b. Me gusta trabajar intensamente",
+  },
+  { pregunta: 81, respuesta: "a. Yo trato de lograr lo que quiero" },
+  {
+    pregunta: 81,
+    respuesta: "b. Soy un buen 'dirigente'",
+  },
+  {
+    pregunta: 82,
+    respuesta: "a. Hago que los demás trabajen intensamente",
+  },
+  {
+    pregunta: 82,
+    respuesta: "b. Soy una persona que no se preocupa por nada",
+  },
+  { pregunta: 83, respuesta: "a. Yo me decido rápidamente" },
+  { pregunta: 83, respuesta: "b. Yo hablo rápidamente" },
+  { pregunta: 84, respuesta: "a. Normalmente trabajo de prisa" },
+  { pregunta: 84, respuesta: "b. Hago ejercicio con regularidad" },
+  {
+    pregunta: 85,
+    respuesta: "a. No me gusta conocer gente",
+  },
+  {
+    pregunta: 85,
+    respuesta: "b. Me canso en seguida",
+  },
+  { pregunta: 86, respuesta: "a. Hago muchísmos amigos" },
+  {
+    pregunta: 86,
+    respuesta: "b. Dedico mucho tiempo a pensar",
+  },
+  {
+    pregunta: 87,
+    respuesta: "a. Me gusta trabajar con problemas teóricos",
+  },
+  {
+    pregunta: 87,
+    respuesta: "b. Me gusta trabajar con detalles",
+  },
+  {
+    pregunta: 88,
+    respuesta: "a. Me gusta trabajar con detalles",
+  },
+  {
+    pregunta: 88,
+    respuesta: "b. Me gusta organizar mi trabajo",
+  },
+  {
+    pregunta: 89,
+    respuesta: "a. Pongo las cosas en su lugar",
+  },
+  {
+    pregunta: 89,
+    respuesta: "b. Siempre soy agradable",
+  },
+  {
+    pregunta: 90,
+    respuesta: "a. Me gusta que me digan lo que tengo que hacer",
+  },
+  {
+    pregunta: 90,
+    respuesta: "b. Yo debo terminar lo que he iniciado",
   },
 ];
+
+module.exports = {
+  pruebaKostick,
+};
