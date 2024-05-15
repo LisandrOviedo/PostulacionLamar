25c39d52356f54b01fb1496deb24376f463ec747
LisandrOviedo <lisandroviedo@hotmail.com>
2024-05-15 16:38:55 -0400

    Renderización de respuestas terminado

--- Client/src/components/TestActitudinal/TestActitudinal.jsx
+++ Client/src/components/TestActitudinal/TestActitudinal.jsx
@@ -31,8 +31,6 @@ export function TestActitudinal() {
 
     dispatch(getPrueba(token));
 
-    console.log(prueba_kostick);
-
     document.title = "Grupo Lamar - Aplicar Test Actitudinal";
 
     return () => {
@@ -44,16 +42,28 @@ export function TestActitudinal() {
     <div className="mt-24 sm:mt-32 h-full flex flex-col px-5 sm:px-10 bg-white">
       <Title>Aplicar Test Actitudinal</Title>
       <hr className="w-[80%] h-0.5 my-5 bg-gray-300 border-0 m-auto" />
-      <div className="grid gap-6 grid-cols-1 md:grid-cols-3 mt-5 mb-5">
-        {/* {(() => {
-          for (const key in prueba_kostick) {
-            prueba_kostick[key].forEach((item, i) => {
-              <div key={i} className="flex flex-col place-content-between">
-                <label htmlFor="">{item.numero_pregunta}</label>
-              </div>;
-            });
-          }
-        })()} */}
+      <div className="grid gap-10 grid-cols-1 md:grid-cols-3 mt-5 mb-5">
+        {prueba_kostick.map((respuestas, index) => (
+          <div key={index + 1} className="flex flex-col gap-4">
+            <label htmlFor={index + 1} className="font-bold">
+              Pregunta número {index + 1}
+            </label>
+            {respuestas.map((respuesta) => (
+              <div key={respuesta.respuesta_id} className="flex gap-2">
+                <input
+                  type="radio"
+                  value={respuesta.respuesta}
+                  id={respuesta.respuesta_id}
+                  name={respuesta.numero_pregunta}
+                />
+                <label htmlFor={respuesta.respuesta_id}>
+                  {respuesta.respuesta}
+                </label>
+              </div>
+            ))}
+          </div>
+        ))}
+
         <div className="md:col-span-3 flex justify-center items-center">
           <Button className="m-0 w-auto">Terminar Test</Button>
         </div>
