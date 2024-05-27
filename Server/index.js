d0d5a9ac3ef5ad210337f94afd5e5c670cf64b6e
LisandrOviedo <lisandroviedo@hotmail.com>
2024-05-27 14:48:40 -0400

    Crear automáticamente respuestas de la prueba kostick al iniciar el servidor

--- Server/index.js
+++ Server/index.js
@@ -4,11 +4,18 @@ const server = require("./src/server.js");
 const { conn } = require("./src/db.js");
 const PORT = process.env.PORT_SERVER || 3001;
 
+const {
+  crearRespuestas,
+} = require("./src/controllers/respuestas_controllers.js");
+
 conn
   .sync({ alter: true })
   .then(() => {
     server.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
+
+      // Registrar respuestas del test kostick en la BD
+      crearRespuestas();
     });
   })
   .catch((error) => console.error(error));
