55e29df405756d866896e8d2ad11b47f23fe0e8f
LisandrOviedo <lisandroviedo@hotmail.com>
2024-07-22 14:58:24 -0400

    Removidas líneas de código innecesarias

--- Server/src/routes/preguntas_kostick_routes.js
+++ Server/src/routes/preguntas_kostick_routes.js
@@ -1,7 +1,6 @@
 const { Router } = require("express");
 const {
   getPreguntasKostick,
-  postPreguntasKostick,
 } = require("../handlers/preguntas_kostick_handlers");
 
 const { authenticateToken } = require("../auth/index");
@@ -9,7 +8,5 @@ const { authenticateToken } = require("../auth/index");
 const preguntas_kostick = Router();
 
 preguntas_kostick.get("/", authenticateToken, getPreguntasKostick);
-
-preguntas_kostick.post("/", postPreguntasKostick);
 
 module.exports = preguntas_kostick;
