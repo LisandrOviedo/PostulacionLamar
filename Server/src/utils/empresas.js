4a0a4d79825a8ff881d5ece46c43d2901b0f6f3a
LisandrOviedo <lisandroviedo@hotmail.com>
2024-08-07 12:42:59 -0400

    Fix: Corrección de nombre de propiedad

--- Server/src/utils/empresas.js
+++ Server/src/utils/empresas.js
@@ -12,7 +12,7 @@ const empresas_faltantes = [
     codigo_empresa: "106",
     descripcion_empresa: "Corporativo",
     direccion_empresa: "AV.5 SAN FRANCISCO SECTOR EL B AJO, FRENTE A  LA CRUZ",
-    rif: "J-30711765-6",
+    rif_empresa: "J-30711765-6",
   },
   // {
   //   descripcion_empresa: "Motores Lago",
