e4c6efd81d25166d052714e44debfd8ec32ecfc3
LisandrOviedo <lisandroviedo@hotmail.com>
2024-08-09 17:11:46 -0400

    Feat: Añadida ruta, handler y controller para generar la ficha de ingreso en formato PDF (en desarrollo)

--- Server/src/controllers/fichas_ingresos_controllers.js
+++ Server/src/controllers/fichas_ingresos_controllers.js
@@ -1,4 +1,23 @@
-const { conn, Fichas_Ingresos, Empleados } = require("../db");
+const {
+  conn,
+  Fichas_Ingresos,
+  Empleados,
+  Cargos_Niveles,
+  Cargos,
+  Departamentos,
+  Empresas,
+  Titulos_Obtenidos,
+  Estados,
+  Paises,
+  Etnias,
+  Experiencias,
+  Salud,
+  Contactos_Emergencia,
+  Datos_Bancarios,
+  Direcciones,
+  Municipios,
+  Parroquias,
+} = require("../db");
 
 const todasLasFichasIngresos = async (empleado_id) => {
   if (!empleado_id) {
@@ -28,8 +47,132 @@ const traerFichaIngresoEmpleado = async (empleado_id) => {
   }
 
   try {
-    const ficha_ingreso = await Fichas_Ingresos.findOne({
-      where: { empleado_id: empleado_id, activo: true },
+    const ficha_ingreso = await Empleados.findByPk(empleado_id, {
+      attributes: {
+        exclude: ["rol_id", "clave", "activo", "createdAt", "updatedAt"],
+      },
+      include: [
+        {
+          model: Titulos_Obtenidos,
+          attributes: {
+            exclude: ["empleado_id", "activo", "createdAt", "updatedAt"],
+          },
+        },
+        {
+          model: Experiencias,
+          attributes: {
+            exclude: ["empleado_id", "activo", "createdAt", "updatedAt"],
+          },
+        },
+        {
+          model: Etnias,
+          attributes: {
+            exclude: ["empleado_id", "activo", "createdAt", "updatedAt"],
+          },
+        },
+        {
+          model: Estados,
+          attributes: {
+            exclude: ["empleado_id", "activo", "createdAt", "updatedAt"],
+          },
+        },
+        {
+          model: Paises,
+          attributes: {
+            exclude: ["empleado_id", "activo", "createdAt", "updatedAt"],
+          },
+        },
+        {
+          model: Salud,
+          attributes: {
+            exclude: ["empleado_id", "activo", "createdAt", "updatedAt"],
+          },
+        },
+        {
+          model: Contactos_Emergencia,
+          attributes: {
+            exclude: ["empleado_id", "activo", "createdAt", "updatedAt"],
+          },
+        },
+        {
+          model: Datos_Bancarios,
+          attributes: {
+            exclude: ["empleado_id", "activo", "createdAt", "updatedAt"],
+          },
+        },
+        {
+          model: Direcciones,
+          attributes: {
+            exclude: [
+              "empleado_id",
+              "parroquia_id",
+              "municipio_id",
+              "estado_id",
+              "pais_id",
+              "activo",
+              "createdAt",
+              "updatedAt",
+            ],
+          },
+          include: [
+            {
+              model: Paises,
+              attributes: ["pais_id", "nombre"],
+            },
+            {
+              model: Estados,
+              attributes: ["estado_id", "nombre"],
+            },
+            {
+              model: Municipios,
+              attributes: ["municipio_id", "nombre"],
+            },
+            {
+              model: Parroquias,
+              attributes: ["parroquia_id", "nombre"],
+            },
+          ],
+        },
+        {
+          model: Cargos_Niveles,
+          attributes: ["cargo_nivel_id", "nivel"],
+          required: true,
+          include: [
+            {
+              model: Cargos,
+              attributes: [
+                "cargo_id",
+                "descripcion",
+                "descripcion_cargo_antiguo",
+              ],
+              include: [
+                {
+                  model: Departamentos,
+                  attributes: ["departamento_id", "nombre"],
+                  include: [
+                    {
+                      model: Empresas,
+                      attributes: ["empresa_id", "nombre"],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+          through: {
+            model: Fichas_Ingresos,
+            attributes: [
+              "ficha_ingreso_id",
+              "salario",
+              "fecha_ingreso",
+              "observaciones",
+            ],
+            where: {
+              activo: true,
+            },
+          },
+        },
+      ],
     });
 
     if (!ficha_ingreso) {
@@ -59,6 +202,225 @@ const traerFichaIngreso = async (ficha_ingreso_id) => {
     return ficha_ingreso;
   } catch (error) {
     throw new Error(`Error al traer la ficha de ingreso: ${error.message}`);
+  }
+};
+
+const traerFichaIngresoPDF = async (empleado_id) => {
+  if (!empleado_id) {
+    throw new Error(`Datos faltantes`);
+  }
+
+  const content = [];
+
+  try {
+    const ficha_ingreso = await traerFichaIngresoEmpleado(empleado_id);
+
+    content.push({
+      titulo: "Información Personal",
+      contenido: [
+        {
+          titulo_campo: "Nombre completo: ",
+          descripcion_campo: `${ficha_ingreso.nombres} ${ficha_ingreso.apellidos}`,
+        },
+        {
+          titulo_campo: "Número de identificación: ",
+          descripcion_campo: `${ficha_ingreso.tipo_identificacion}${ficha_ingreso.numero_identificacion}`,
+        },
+        {
+          titulo_campo: "Estado civil: ",
+          descripcion_campo: `${ficha_ingreso.estado_civil}`,
+        },
+        {
+          titulo_campo: "Registro de información fiscal (RIF): ",
+          descripcion_campo: `${ficha_ingreso.rif}`,
+        },
+        {
+          titulo_campo: "Número de teléfono: ",
+          descripcion_campo: ficha_ingreso.telefono,
+        },
+        {
+          titulo_campo: "Correo electrónico: ",
+          descripcion_campo: ficha_ingreso.correo,
+        },
+        {
+          titulo_campo: "Etnia: ",
+          descripcion_campo: ficha_ingreso.Etnia?.nombre || null,
+        },
+        {
+          titulo_campo: "Mano dominante: ",
+          descripcion_campo: ficha_ingreso.mano_dominante,
+        },
+        {
+          titulo_campo: "Sexo: ",
+          descripcion_campo: ficha_ingreso.sexo,
+        },
+        {
+          titulo_campo: "Factor y grupo sanguíneo: ",
+          descripcion_campo: ficha_ingreso.factor_grupo_sanguineo,
+        },
+        {
+          titulo_campo: "Cantidad hijos: ",
+          descripcion_campo: ficha_ingreso.cantidad_hijos,
+        },
+        {
+          titulo_campo: "Carga familiar: ",
+          descripcion_campo: ficha_ingreso.carga_familiar,
+        },
+        {
+          titulo_campo: "Fecha de nacimiento: ",
+          descripcion_campo: `${ficha_ingreso.fecha_nacimiento} (${calcularEdad(
+            ficha_ingreso.fecha_nacimiento
+          )} años)`,
+        },
+        {
+          titulo_campo: "Lugar de nacimiento: ",
+          descripcion_campo: ficha_ingreso.nacimiento_lugar,
+        },
+        {
+          titulo_campo: "Estado de nacimiento: ",
+          descripcion_campo: ficha_ingreso.Estado.nombre,
+        },
+        {
+          titulo_campo: "Nacionalidad: ",
+          descripcion_campo: ficha_ingreso.Paise.nombre,
+        },
+        {
+          titulo_campo: "Licencia de conducir: ",
+          descripcion_campo: ficha_ingreso.licencia_conducir_grado
+            ? `Grado ${ficha_ingreso.licencia_conducir_grado}`
+            : null,
+        },
+        {
+          titulo_campo: "Fecha de vencimiento de licencia: ",
+          descripcion_campo:
+            ficha_ingreso.licencia_conducir_vencimiento || null,
+        },
+        {
+          titulo_campo: "Carta médica: ",
+          descripcion_campo: ficha_ingreso.carta_medica_vencimiento || null,
+        },
+        {
+          titulo_campo: "Talla camisa: ",
+          descripcion_campo: ficha_ingreso.talla_camisa,
+        },
+        {
+          titulo_campo: "Talla pantalón: ",
+          descripcion_campo: ficha_ingreso.talla_pantalon,
+        },
+        {
+          titulo_campo: "Talla calzado: ",
+          descripcion_campo: ficha_ingreso.talla_calzado,
+        },
+        {
+          titulo_campo: "¿Trabajó anteriormente en esta empresa?: ",
+          descripcion_campo: ficha_ingreso.trabajo_anteriormente_especifique
+            ? ficha_ingreso.trabajo_anteriormente_especifique
+            : "No",
+        },
+        {
+          titulo_campo: "Motivo del retiro: ",
+          descripcion_campo: ficha_ingreso.motivo_retiro || null,
+        },
+        {
+          titulo_campo: "¿Posee parientes que trabajen en esta empresa?: ",
+          descripcion_campo: ficha_ingreso.posee_parientes_empresa,
+        },
+      ],
+    });
+
+    content.push({
+      titulo: "Dirección de Habitación",
+      contenido: [
+        {
+          titulo_campo: "Calle / Avenida: ",
+          descripcion_campo: ficha_ingreso.Direcciones.calle_avenida,
+        },
+        {
+          titulo_campo: "Tipo vivienda: ",
+          descripcion_campo: ficha_ingreso.Direcciones.tipo_vivienda,
+        },
+        {
+          titulo_campo: "Número casa: ",
+          descripcion_campo: ficha_ingreso.Direcciones.numero_casa || null,
+        },
+        {
+          titulo_campo: "Piso: ",
+          descripcion_campo: ficha_ingreso.Direcciones.piso || null,
+        },
+        {
+          titulo_campo: "Apartamento: ",
+          descripcion_campo: ficha_ingreso.Direcciones.apartamento || null,
+        },
+        {
+          titulo_campo: "Urbanización / Sector: ",
+          descripcion_campo: ficha_ingreso.Direcciones.urbanizacion_sector,
+        },
+        {
+          titulo_campo: "Parroqia: ",
+          descripcion_campo: ficha_ingreso.Direcciones.Parroquia.nombre,
+        },
+        {
+          titulo_campo: "Municipio: ",
+          descripcion_campo: ficha_ingreso.Direcciones.Municipio.nombre,
+        },
+        {
+          titulo_campo: "Estado: ",
+          descripcion_campo: ficha_ingreso.Direcciones.Estado.nombre,
+        },
+        {
+          titulo_campo: "País: ",
+          descripcion_campo: ficha_ingreso.Direcciones.Paise.nombre,
+        },
+      ],
+    });
+
+    let titulos_obtenidos = [];
+
+    ficha_ingreso.Titulos_Obtenidos.forEach((titulo_obtenido) => {
+      titulos_obtenidos.push({
+        grado_instruccion: titulo_obtenido.grado_instruccion,
+        fecha_desde: titulo_obtenido.fecha_desde,
+        fecha_hasta: titulo_obtenido.fecha_hasta,
+        nombre_instituto: titulo_obtenido.nombre_instituto,
+        titulo_obtenido: titulo_obtenido.titulo_obtenido,
+      });
+    });
+
+    content.push({
+      titulo: "Títulos Obtenidos",
+      contenido: [
+        {
+          titulo_campo: "Títulos Obtenidos",
+          descripcion_campo: titulos_obtenidos,
+        },
+      ],
+    });
+
+    let experiencias = [];
+
+    ficha_ingreso.Experiencias.forEach((experiencia) => {
+      experiencias.push({
+        tipo: experiencia.tipo,
+        cargo_titulo: experiencia.cargo_titulo,
+        fecha_desde: experiencia.fecha_desde,
+        fecha_hasta: experiencia.fecha_hasta,
+        empresa_centro_educativo: experiencia.empresa_centro_educativo,
+      });
+    });
+
+    content.push({
+      titulo: "Trabajos Anteriores",
+      contenido: [
+        {
+          titulo_campo: "Trabajos Anteriores",
+          descripcion_campo: experiencias,
+        },
+      ],
+    });
+
+    return content;
+  } catch (error) {
+    throw new Error(`Error al traer el curriculo: ${error.message}`);
   }
 };
 
@@ -181,8 +543,9 @@ const inactivarFichaIngreso = async (ficha_ingreso_id) => {
 
 module.exports = {
   todasLasFichasIngresos,
-  traerFichaIngreso,
   traerFichaIngresoEmpleado,
+  traerFichaIngreso,
+  traerFichaIngresoPDF,
   crearFichaIngreso,
   modificarFichaIngreso,
   inactivarFichaIngreso,
--- Server/src/handlers/fichas_ingresos_handlers.js
+++ Server/src/handlers/fichas_ingresos_handlers.js
@@ -2,6 +2,7 @@ const {
   todasLasFichasIngresos,
   traerFichaIngresoEmpleado,
   traerFichaIngreso,
+  traerFichaIngresoPDF,
   crearFichaIngreso,
   modificarFichaIngreso,
   inactivarFichaIngreso,
@@ -55,6 +56,199 @@ const postFichaIngreso = async (req, res) => {
   }
 };
 
+const getFichaIngresoPDF = async (req, res) => {
+  const { empleado_id, identificacion } = req.body;
+  const filename = "Ficha Ingreso.pdf";
+
+  try {
+    const doc = new PDFDocument({
+      bufferPages: true,
+      font: "Helvetica",
+    });
+
+    // Genera el contenido del PDF
+    const content = await traerFichaIngresoPDF(empleado_id);
+
+    const dest_path = path.join(
+      __dirname,
+      `../../public/documentosEmpleados/${identificacion}`
+    );
+
+    crearCarpetaSiNoExiste(dest_path);
+
+    const pdf_path = path.join(
+      __dirname,
+      `../../public/documentosEmpleados/${identificacion}/${filename}`
+    );
+
+    doc.pipe(fs.createWriteStream(pdf_path));
+
+    const logoPath = path.join(__dirname, `../../public/LogoAzul.png`);
+    doc.image(logoPath, 55, 35, { width: 80 });
+
+    doc.moveDown(2);
+    doc
+      .fontSize(14)
+      .font("Helvetica-Bold")
+      .text("Ficha Ingreso", { align: "center" });
+    doc.moveDown();
+
+    doc.on("pageAdded", () => {
+      doc.fillColor("black");
+      doc.image(logoPath, 55, 35, { width: 80 });
+      doc.moveDown(2);
+      doc.moveDown();
+    });
+
+    for (const seccion of content) {
+      doc
+        .fontSize(12)
+        .font("Helvetica-Bold")
+        .text(seccion.titulo, { underline: true });
+      doc.moveDown();
+      // AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+      for (const campo of seccion.contenido) {
+        if (campo.titulo_campo === "Experiencias") {
+          if (!campo.descripcion_campo.length) {
+            doc
+              .fontSize(11)
+              .font("Helvetica")
+              .text("No tiene experencia previa", { indent: 20 });
+          } else {
+            (async function createTable() {
+              const table = {
+                headers: [
+                  "Tipo",
+                  "Cargo / Título",
+                  "Desde",
+                  "Hasta",
+                  "Empresa / Centro Educativo",
+                ],
+                rows: [],
+              };
+              for (const experiencia of campo.descripcion_campo) {
+                const row = [
+                  experiencia.tipo,
+                  experiencia.cargo_titulo,
+                  experiencia.fecha_desde,
+                  experiencia.fecha_hasta,
+                  experiencia.empresa_centro_educativo,
+                ];
+                table.rows.push(row);
+              }
+              await doc.table(table, {
+                columnsSize: [50, 130, 70, 70, 160],
+                prepareHeader: () => doc.fontSize(11).font("Helvetica-Bold"),
+                prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+                  doc.fontSize(10).font("Helvetica");
+                },
+              });
+            })();
+          }
+        } else if (campo.titulo_campo === "Títulos Obtenidos") {
+          if (!campo.descripcion_campo.length) {
+            doc
+              .fontSize(11)
+              .font("Helvetica")
+              .text("No tiene títulos", { indent: 20 });
+          } else {
+            (async function createTable2() {
+              const table = {
+                headers: [
+                  "Grado Instrucción",
+                  "Desde",
+                  "Hasta",
+                  "Nombre Instituto",
+                  "Título Obtenido",
+                ],
+                rows: [],
+              };
+              for (const titulo of campo.descripcion_campo) {
+                const row = [
+                  titulo.grado_instruccion,
+                  titulo.fecha_desde,
+                  titulo.fecha_hasta,
+                  titulo.nombre_instituto,
+                  titulo.titulo_obtenido,
+                ];
+                table.rows.push(row);
+              }
+              await doc.table(table, {
+                columnsSize: [110, 70, 70, 115, 115],
+                prepareHeader: () => doc.fontSize(11).font("Helvetica-Bold"),
+                prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+                  doc.fontSize(10).font("Helvetica");
+                },
+              });
+            })();
+          }
+        } else if (campo.titulo_campo === "Idiomas") {
+          if (!campo.descripcion_campo.length) {
+            doc
+              .fontSize(11)
+              .font("Helvetica")
+              .text("No tiene conocimientos", { indent: 20 });
+          } else {
+            (async function createTable() {
+              const table = {
+                headers: ["Idioma", "Nivel"],
+                rows: [],
+              };
+              for (const idioma of campo.descripcion_campo) {
+                const row = [idioma.nombre, idioma.nivel];
+                table.rows.push(row);
+              }
+              await doc.table(table, {
+                prepareHeader: () => doc.fontSize(11).font("Helvetica-Bold"),
+                prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
+                  doc.fontSize(10).font("Helvetica");
+                },
+              });
+            })();
+          }
+        } else {
+          if (campo.titulo_campo) {
+            doc.fontSize(11).font("Helvetica-Bold").text(campo.titulo_campo, {
+              continued: true,
+              indent: 20,
+            });
+
+            if (!campo.descripcion_campo) {
+              doc
+                .fontSize(11)
+                .font("Helvetica")
+                .text("Sin registrar / No posee", { indent: 20 });
+            } else {
+              doc.fontSize(11).font("Helvetica").text(campo.descripcion_campo);
+            }
+          } else {
+            if (!campo.descripcion_campo) {
+              doc
+                .fontSize(11)
+                .font("Helvetica")
+                .text("Sin registrar / No posee", { indent: 20 });
+            } else {
+              doc
+                .fontSize(11)
+                .font("Helvetica")
+                .text(campo.descripcion_campo, { indent: 20 });
+            }
+          }
+        }
+      }
+      doc.moveDown();
+    }
+
+    doc.end();
+
+    await crearCurriculoPDF(empleado_id, filename, pdf_path);
+
+    return res.status(201).json(filename);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
 const putFichaIngreso = async (req, res) => {
   const {
     ficha_ingreso_id,
@@ -95,6 +289,7 @@ module.exports = {
   getFichasIngresosEmpleado,
   getFichaIngresoEmpleado,
   getFichaIngreso,
+  getFichaIngresoPDF,
   postFichaIngreso,
   putFichaIngreso,
   deleteFichaIngreso,
