7ff216f7d0076aa40a0dc7d43e4bb23448fed648
LisandrOviedo <lisandroviedo@hotmail.com>
2024-08-14 10:27:49 -0400

    Feat: Añadida librería disable-react-devtools para entorno de producción

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,6 +6,11 @@ import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
