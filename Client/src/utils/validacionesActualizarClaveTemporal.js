553cfd2a7965c7d551f539eaafd1986ecd3e38d7
LisandrOviedo <lisandroviedo@hotmail.com>
2024-09-05 16:24:13 -0400

    Refactor: Disminución de líneas de código

--- Client/src/utils/validacionesActualizarClaveTemporal.js
+++ Client/src/utils/validacionesActualizarClaveTemporal.js
@@ -1,22 +1,24 @@
 const regex = /^\d+$/;
 
 export default function validations(inputs) {
+  const { clave, confirmarClave } = inputs;
+
   const errors = {};
 
-  if (inputs.clave) {
-    if (!regex.test(inputs.clave)) {
+  if (clave) {
+    if (!regex.test(clave)) {
       errors.clave = "Debe ingresar solo números";
-    } else if (inputs.clave.length !== 4) {
+    } else if (clave.length !== 4) {
       errors.clave = "Su clave debe ser de 4 dígitos";
     }
   }
 
-  if (inputs.confirmarClave) {
-    if (!regex.test(inputs.confirmarClave)) {
+  if (confirmarClave) {
+    if (!regex.test(confirmarClave)) {
       errors.confirmarClave = "Debe ingresar solo números";
-    } else if (inputs.confirmarClave.length !== 4) {
+    } else if (confirmarClave.length !== 4) {
       errors.confirmarClave = "Su clave debe ser de 4 dígitos";
-    } else if (inputs.clave !== inputs.confirmarClave) {
+    } else if (clave !== confirmarClave) {
       errors.confirmarClave =
         "La clave y la confirmación de clave deben ser iguales";
     }
