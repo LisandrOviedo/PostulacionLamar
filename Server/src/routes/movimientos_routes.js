9b5021285c2fb74c70336df55b74e904f9466603
LisandrOviedo <lisandroviedo@hotmail.com>
2024-09-27 10:16:10 -0400

    Feat: Añadidas rutas, handlers y controllers para aprobar y denegar un movimiento.

--- Server/src/routes/movimientos_routes.js
+++ Server/src/routes/movimientos_routes.js
@@ -4,6 +4,8 @@ const {
   getMovimiento,
   postMovimiento,
   putMovimiento,
+  putAprobarMovimiento,
+  putDenegarMovimiento,
   deleteMovimiento,
 } = require("../handlers/movimientos_handlers");
 
@@ -17,6 +19,8 @@ movimientos.post("/", authenticateToken, postMovimiento);
 movimientos.post("/allMovimientos", authenticateToken, getMovimientos);
 
 movimientos.put("/modificar", authenticateToken, putMovimiento);
+movimientos.put("/aprobar", authenticateToken, putAprobarMovimiento);
+movimientos.put("/denegar", authenticateToken, putDenegarMovimiento);
 movimientos.put("/inactivar", authenticateToken, deleteMovimiento);
 
 module.exports = movimientos;
