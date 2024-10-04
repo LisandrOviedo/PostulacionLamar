00fb7489ebe617c263737e0b4cf25c780eeeb42d
Astridmcf <moleroastridcarolina@gmail.com>
2024-10-04 16:01:25 -0400

    Fix: Se hizo una correción en la validación del correo electrónico para que solo lo tome como invalido si no contiene el caracter arroba (@) y se anadió una condición en la validación del teléfono que si esta vacio no muestre el mensaje de error.

--- Client/src/utils/validacionesDatosPersonales.js
+++ Client/src/utils/validacionesDatosPersonales.js
@@ -1,34 +1,35 @@
-const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const regexTelefono = /^\+[1-9]\d*$/;
+
+const soloNumeros = /^[0-9]+$/;
 
 export default function validations(inputs) {
-  const { telefono } = inputs;
-
+  const { telefono, correo, direccion } = inputs;
+ 
   const errors = {};
 
-  if (telefono) {
-    if (!regexTelefono.test(telefono)) {
-      errors.telefono = "Teléfono inválido (Ejemplo: +58412XXXXXXX)";
-    } else if (telefono.length < 11) {
-      errors.telefono = "El teléfono debe contener al menos 11 caracteres";
-    } else if (telefono.length > 20) {
-      errors.telefono = "El teléfono debe contener máximo 20 caracteres";
-    }
+  // Validación del teléfono
+  if (telefono.trim() === "") {
+    // No hay error si el campo está vacío
+  } else if (!soloNumeros.test(telefono)) {
+    errors.telefono = "Solo debe contener números";
+  } else if (telefono.length < 11) {
+    errors.telefono = "Debe contener al menos 11 caracteres.";
+  } else if (telefono.length > 20) {
+    errors.telefono = "Debe contener máximo 20 caracteres.";
   }
 
+  // Validación del correo
   if (correo) {
-    if (!regexEmail.test(correo)) {
-      errors.correo =
-        "Correo electrónico inválido (Ejemplo: ejemplo@ejemplo.com)";
+    if (!correo.includes('@')) {
+      errors.correo = "Correo electrónico inválido: debe contener un '@'.";
     } else if (correo.length > 150) {
-      errors.correo =
-        "El correo electrónico debe contener máximo 150 caracteres";
+      errors.correo = "El máximo de caracteres en el correo son 150.";
     }
   }
 
+  // Validación de la dirección
   if (direccion) {
     if (direccion.length > 150) {
-      errors.direccion = "La dirección debe contener máximo 150 caracteres";
+      errors.direccion = "La dirección debe contener máximo 150 caracteres.";
     }
   }
 
