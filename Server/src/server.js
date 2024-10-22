11a433f21d9c6797248edbd621adb1dfdca8c78e
LisandrOviedo <lisandroviedo@hotmail.com>
2024-10-22 16:21:16 -0400

    Feat: Eliminación de header innecesario que muestra información sobre la tecnología utilizada para el servidor

--- Server/src/server.js
+++ Server/src/server.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 
 const server = express();
 
+server.disable("x-powered-by");
+
 server.use(cors());
 server.use(morgan("dev"));
 server.use(express.json({ limit: "65mb" })); //Límite máximo en el tamaño de los datos JSON que el servidor puede manejar de una sola vez, para evitar posibles ataques de denegación de servicio (DoS) o abusos.
