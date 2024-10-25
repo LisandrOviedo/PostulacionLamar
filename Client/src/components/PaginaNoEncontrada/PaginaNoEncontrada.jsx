a50740cf2533ef09d332661200e6f9f99da1eb72
LisandrOviedo <lisandroviedo@hotmail.com>
2024-10-25 11:46:05 -0400

    Feat: La página no encontrada ahora te arroja un mensaje en pantalla con un botón para volver a la página de atrás en vez de hacerlo automáticamente sin mensaje

--- Client/src/components/PaginaNoEncontrada/PaginaNoEncontrada.jsx
+++ Client/src/components/PaginaNoEncontrada/PaginaNoEncontrada.jsx
@@ -1,13 +1,26 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { Button, Title, Hr } from "../UI";
+
 export function PaginaNoEncontrada() {
   const navigate = useNavigate();
 
   useEffect(() => {
     window.scroll(0, 0);
-    navigate(-1);
   }, []);
 
-  return <div></div>;
+  return (
+    <div className="mt-24 sm:mt-32 flex min-h-full flex-1 flex-col items-center px-6 lg:px-8 mb-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Title>Página no encontrada</Title>
+      </div>
+      <br />
+      <Hr />
+      <br />
+      <Button className="m-0 w-auto" onClick={() => navigate(-1)}>
+        Volver
+      </Button>
+    </div>
+  );
 }
