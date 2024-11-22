2f050856b566eb2742c6b1517466e6a508c5648e
LisandrOviedo <lisandroviedo@hotmail.com>
2024-11-22 13:20:50 -0400

    Refactor: Eliminación de líneas de código innecesarias.
    Feat: Suspense ahora envuelve main en lugar de las rutas de app.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -43,127 +43,119 @@ import {
   ProteccionEmpleado,
 } from "./ProteccionRutas/";
 
-import { Suspense } from "react";
-
 function App() {
   const { pathname } = useLocation();
   const userState = useSelector((state) => state.empleados.empleado);
 
   return (
-    <div>
-      <Suspense fallback={<>Cargando</>}>
-        {pathname.toLowerCase() !== "/" &&
-          pathname.toLowerCase() !== "/admin/acceso" &&
-          pathname.toLocaleLowerCase() !==
-            "/miperfil/actualizarclavetemporal" &&
-          pathname.toLocaleLowerCase() !== "/sugerencias" &&
-          !pathname.toLocaleLowerCase().startsWith("/infopostulacion/") &&
-          userState.empleado_id && <BarraNavegacion />}
-        <Routes>
-          <Route path="/" element={<AccesoEmpleado />} />
-          <Route path="/admin/acceso" element={<AccesoAdmin />} />
-          <Route path="/sugerencias" element={<EnviarSugerencia />} />
+    <>
+      {pathname.toLowerCase() !== "/" &&
+        pathname.toLowerCase() !== "/admin/acceso" &&
+        pathname.toLocaleLowerCase() !== "/miperfil/actualizarclavetemporal" &&
+        pathname.toLocaleLowerCase() !== "/sugerencias" &&
+        !pathname.toLocaleLowerCase().startsWith("/infopostulacion/") &&
+        userState.empleado_id && <BarraNavegacion />}
+      <Routes>
+        <Route path="/" element={<AccesoEmpleado />} />
+        <Route path="/admin/acceso" element={<AccesoAdmin />} />
+        <Route path="/sugerencias" element={<EnviarSugerencia />} />
+        <Route
+          path="/infoPostulacion/:vacante_id"
+          element={<InfoPostulacion />}
+        />
+
+        <Route element={<ProteccionActualizarClave />}>
           <Route
-            path="/infoPostulacion/:vacante_id"
-            element={<InfoPostulacion />}
+            path="/miPerfil/actualizarClaveTemporal"
+            element={<ActualizarClaveTemporal />}
           />
+        </Route>
 
-          <Route element={<ProteccionActualizarClave />}>
-            <Route
-              path="/miPerfil/actualizarClaveTemporal"
-              element={<ActualizarClaveTemporal />}
-            />
-          </Route>
+        {/* Rutas Empleado */}
+        <Route element={<ProteccionEmpleado />}>
+          <Route path="/inicio" element={<Inicio />} />
+          <Route
+            path="/miPerfil/datosPersonales"
+            element={<DatosPersonales />}
+          />
+          <Route
+            path="/miPerfil/actualizarClave"
+            element={<ActualizarClave />}
+          />
+          <Route path="/perfilProfesional/info" element={<InfoCurriculo />} />
+          <Route
+            path="/perfilProfesional/registro"
+            element={<CrearCurriculo />}
+          />
+          <Route
+            path="/perfilProfesional/misDocumentos"
+            element={<SubirDocumentos />}
+          />
+          <Route
+            path="/perfilProfesional/pruebaKostick"
+            element={<PruebaKostick />}
+          />
+          <Route
+            path="/perfilProfesional/misPostulaciones"
+            element={<HistorialPostulaciones />}
+          />
+        </Route>
 
-          {/* Rutas Empleado */}
-          <Route element={<ProteccionEmpleado />}>
-            <Route path="/inicio" element={<Inicio />} />
-            <Route
-              path="/miPerfil/datosPersonales"
-              element={<DatosPersonales />}
-            />
-            <Route
-              path="/miPerfil/actualizarClave"
-              element={<ActualizarClave />}
-            />
-            <Route path="/perfilProfesional/info" element={<InfoCurriculo />} />
-            <Route
-              path="/perfilProfesional/registro"
-              element={<CrearCurriculo />}
-            />
-            <Route
-              path="/perfilProfesional/misDocumentos"
-              element={<SubirDocumentos />}
-            />
-            <Route
-              path="/perfilProfesional/pruebaKostick"
-              element={<PruebaKostick />}
-            />
-            <Route
-              path="/perfilProfesional/misPostulaciones"
-              element={<HistorialPostulaciones />}
-            />
-          </Route>
+        {/* Rutas Admin */}
+        <Route element={<ProteccionAdmin />}>
+          <Route path="/admin/panel" element={<PanelAdmin />} />
+          <Route
+            path="/admin/miPerfil/datosPersonales"
+            element={<DatosPersonales />}
+          />
+          <Route
+            path="/admin/miPerfil/actualizarClave"
+            element={<ActualizarClave />}
+          />
+          <Route
+            path="/admin/perfilesProfesionales"
+            element={<PerfilesProfesionales />}
+          />
+          <Route path="/admin/empleados" element={<Empleados />} />
+          <Route
+            path="/admin/empleados/:empleado_id"
+            element={<DetalleEmpleado />}
+          />
+          <Route
+            path="/admin/pruebasEmpleados"
+            element={<PruebasEmpleados />}
+          />
+          <Route
+            path="/admin/formularioIngreso"
+            element={<FormularioIngreso />}
+          />
+          <Route path="/admin/movimientos" element={<Movimientos />} />
+          <Route
+            path="/admin/solicitudesMovimientos"
+            element={<SolicitudesMovimientos />}
+          />
+          <Route path="/admin/liquidaciones" element={<Liquidaciones />} />
+          <Route path="/admin/gestionRoles" element={<GestionRoles />} />
+          <Route path="/admin/asignacionRoles" element={<AsignacionRoles />} />
+          <Route path="/admin/sugerencias" element={<Sugerencias />} />
+          <Route path="/admin/vacantes" element={<Vacantes />} />
+          <Route
+            path="/admin/vacantes/:vacante_id"
+            element={<DetalleVacante />}
+          />
+          <Route
+            path="/admin/asignacionAccesosEmpresas"
+            element={<AsignacionAccesoEmpresas />}
+          />
+          <Route
+            path="/admin/consultarLiquidaciones"
+            element={<ConsultarLiquidaciones />}
+          />
+        </Route>
 
-          {/* Rutas Admin */}
-          <Route element={<ProteccionAdmin />}>
-            <Route path="/admin/panel" element={<PanelAdmin />} />
-            <Route
-              path="/admin/miPerfil/datosPersonales"
-              element={<DatosPersonales />}
-            />
-            <Route
-              path="/admin/miPerfil/actualizarClave"
-              element={<ActualizarClave />}
-            />
-            <Route
-              path="/admin/perfilesProfesionales"
-              element={<PerfilesProfesionales />}
-            />
-            <Route path="/admin/empleados" element={<Empleados />} />
-            <Route
-              path="/admin/empleados/:empleado_id"
-              element={<DetalleEmpleado />}
-            />
-            <Route
-              path="/admin/pruebasEmpleados"
-              element={<PruebasEmpleados />}
-            />
-            <Route
-              path="/admin/formularioIngreso"
-              element={<FormularioIngreso />}
-            />
-            <Route path="/admin/movimientos" element={<Movimientos />} />
-            <Route
-              path="/admin/solicitudesMovimientos"
-              element={<SolicitudesMovimientos />}
-            />
-            <Route path="/admin/liquidaciones" element={<Liquidaciones />} />
-            <Route path="/admin/gestionRoles" element={<GestionRoles />} />
-            <Route
-              path="/admin/asignacionRoles"
-              element={<AsignacionRoles />}
-            />
-            <Route path="/admin/sugerencias" element={<Sugerencias />} />
-            <Route path="/admin/vacantes" element={<Vacantes />} />
-            <Route
-              path="/admin/vacantes/:vacante_id"
-              element={<DetalleVacante />}
-            />
-            <Route
-              path="/admin/asignacionAccesosEmpresas"
-              element={<AsignacionAccesoEmpresas />}
-            />
-            <Route
-              path="/admin/consultarLiquidaciones"
-              element={<ConsultarLiquidaciones />}
-            />
-          </Route>
-          
-          <Route path="*" element={<PaginaNoEncontrada />} />
-        </Routes>
-      </Suspense>
-    </div>
+        <Route path="*" element={<PaginaNoEncontrada />} />
+      </Routes>
+    </>
   );
 }
 
